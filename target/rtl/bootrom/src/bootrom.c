8c1cc3a8f6e3d818b13482d823c3d8e19f551991
IveanEx <yunhao.deng@kuleuven.be>
2024-11-29 17:22:54 +0100

    Update Bootrom

--- target/rtl/bootrom/src/bootrom.c
+++ target/rtl/bootrom/src/bootrom.c
@@ -12,6 +12,8 @@
 // For values need to share between functions, use uninitialized global variable
 // + initialization function
 
+#include "chip_id.h"
+#include "sys_dma.h"
 #include "uart.h"
 #include "xmodem.h"
 
@@ -30,52 +32,133 @@ void delay_cycles(uint64_t cycle) {
 }
 
 // Boot modes.
-enum boot_mode_t { JTAG, UART, PRINTMEM, NORMAL };
+enum boot_mode_t {
+    TARGET_CHIPID,
+    UART,
+    COPY_TO_REMOTE,
+    COPY_FROM_REMOTE,
+    PRINTMEM,
+    NORMAL
+};
 
 void bootrom() {
-    enum boot_mode_t boot_mode = JTAG;
-    uint64_t start_address;
+    enum boot_mode_t boot_mode = NORMAL;
+    uint64_t local_chip_mem_start_address;
+    uint64_t remote_chip_mem_start_address;
     uint64_t memory_length;
 
-    uint32_t chip_id;
-    asm volatile("csrr %0, 0xf15" : "=r"(chip_id));
+    uint32_t chip_id = get_current_chip_id();
+    uint32_t target_chip_id = chip_id;
     uintptr_t address_prefix = ((uintptr_t)chip_id) << 40;
 
     char in_buf[8];
     init_uart(address_prefix, 50000000, 1000000);
 
     while (1) {
-        start_address = 0x80000000L | ((uint64_t)address_prefix);
+        local_chip_mem_start_address = 0x80000000L | ((uint64_t)address_prefix);
+        remote_chip_mem_start_address =
+            0x80000000L | ((uint64_t)target_chip_id << 40);
         print_str(address_prefix, "\033[2J");
         print_str(address_prefix, "\r\n\t\t Welcome to HeMAiA Bootrom");
         print_str(address_prefix, "\r\n");
         print_str(address_prefix, "\r\n\t Chip ID: 0x");
         print_u8(address_prefix, chip_id);
+        print_str(address_prefix, "\r\n\t Target Remote Chip ID: 0x");
+        print_u8(address_prefix, target_chip_id);
         print_str(address_prefix,
                   "\r\n\t Enter the number to select the mode: ");
-        print_str(address_prefix, "\r\n\t 1. Load from JTAG");
+        print_str(address_prefix, "\r\n\t 1. Change the target remote Chip ID");
         print_str(address_prefix, "\r\n\t 2. Load from UART to 0x");
-        print_u48(address_prefix, start_address);
-        print_str(address_prefix, "\r\n\t 3. Print memory from 0x");
-        print_u48(address_prefix, start_address);
-        print_str(address_prefix, "\r\n\t 4. Continue to Boot from 0x");
-        print_u48(address_prefix, start_address);
+        print_u48(address_prefix, remote_chip_mem_start_address);
+        print_str(address_prefix,
+                  "\r\n\t 3. Copy memory from local chip to remote chip");
+        print_str(address_prefix,
+                  "\r\n\t 4. Copy memory from remote chip to local chip");
+        print_str(address_prefix, "\r\n\t 5. Print memory from 0x");
+        print_u48(address_prefix, remote_chip_mem_start_address);
+        print_str(address_prefix, "\r\n\t 6. Continue to Boot from 0x");
+        print_u48(address_prefix, local_chip_mem_start_address);
         print_str(address_prefix, "\r\n");
 
         boot_mode = getchar(address_prefix) - '0' - 1;
 
+        char* cur = 0;
+
         switch (boot_mode) {
-            case JTAG:
+            case TARGET_CHIPID:
                 print_str(address_prefix,
-                          "\r\n\t Handover to debugger... \r\n\r\n");
-                __asm__ volatile(
-                    "csrr a0, mhartid;"
-                    "ebreak;");
+                          "\r\n\t Enter the target remote Chip ID: ");
+                scan_uart(address_prefix, in_buf);
+                cur = in_buf;
+                target_chip_id = 0;
+                while (*cur != '\0') {
+                    if (*cur >= '0' || *cur <= '9') {
+                        target_chip_id = (target_chip_id << 4) + *cur - '0';
+                    } else if (*cur >= 'A' || *cur <= 'F') {
+                        target_chip_id =
+                            (target_chip_id << 4) + *cur - 'A' + 10;
+                    } else if (*cur >= 'a' || *cur <= 'f') {
+                        target_chip_id =
+                            (target_chip_id << 4) + *cur - 'a' + 10;
+                    } else {
+                        print_str(address_prefix, "\r\n\t Invalid input. ");
+                        getchar(address_prefix);
+                        break;
+                    }
+                    cur++;
+                }
+                break;
+
+            case COPY_TO_REMOTE:
+                print_str(address_prefix,
+                          "\r\n\t Enter the size of the memory in byte: ");
+                scan_uart(address_prefix, in_buf);
+                cur = in_buf;
+                memory_length = 0;
+                while (*cur != '\0') {
+                    memory_length = memory_length * 10 + *cur - '0';
+                    cur++;
+                }
+                print_str(address_prefix, "\r\n\t Copying memory from 0x");
+                print_u48(address_prefix, local_chip_mem_start_address);
+                print_str(address_prefix, " to 0x");
+                print_u48(address_prefix, remote_chip_mem_start_address);
+                print_str(address_prefix, " with size ");
+                print_u48(address_prefix, memory_length);
+                print_str(address_prefix, " bytes...");
+                sys_dma_blk_memcpy(remote_chip_mem_start_address,
+                                   local_chip_mem_start_address, memory_length);
+                print_str(address_prefix, "\r\n\t Copy finished. ");
+                getchar(address_prefix);
+                break;
+
+            case COPY_FROM_REMOTE:
+                print_str(address_prefix,
+                          "\r\n\t Enter the size of the memory in byte: ");
+                scan_uart(address_prefix, in_buf);
+                cur = in_buf;
+                memory_length = 0;
+                while (*cur != '\0') {
+                    memory_length = memory_length * 10 + *cur - '0';
+                    cur++;
+                }
+                print_str(address_prefix, "\r\n\t Copying memory from 0x");
+                print_u48(address_prefix, remote_chip_mem_start_address);
+                print_str(address_prefix, " to 0x");
+                print_u48(address_prefix, local_chip_mem_start_address);
+                print_str(address_prefix, " with size ");
+                print_u48(address_prefix, memory_length);
+                print_str(address_prefix, " bytes...");
+                sys_dma_blk_memcpy(local_chip_mem_start_address,
+                                   remote_chip_mem_start_address,
+                                   memory_length);
+                print_str(address_prefix, "\r\n\t Copy finished. ");
+                getchar(address_prefix);
                 break;
 
             case UART:
                 delay_cycles(50000000);  // Delay for 1s
-                uart_xmodem(address_prefix, start_address);
+                uart_xmodem(address_prefix, remote_chip_mem_start_address);
                 print_str(address_prefix, "\r\n\t Load finished. \r\n\r\n");
                 break;
 
@@ -90,9 +173,10 @@ void bootrom() {
                     cur++;
                 }
                 print_str(address_prefix, "\r\n\t The memory from 0x");
-                print_u48(address_prefix, start_address);
+                print_u48(address_prefix, remote_chip_mem_start_address);
                 print_str(address_prefix, " is:");
-                print_mem_hex(address_prefix, (char*)start_address,
+                print_mem_hex(address_prefix,
+                              (char*)remote_chip_mem_start_address,
                               memory_length);
                 print_str(address_prefix, "\r\n\r\n\t Print finished. ");
                 getchar(address_prefix);
@@ -101,7 +185,7 @@ void bootrom() {
             case NORMAL:
                 print_str(address_prefix, "\033[2J");
                 print_str(address_prefix, "\r\n\t Booting at 0x");
-                print_u48(address_prefix, start_address);
+                print_u48(address_prefix, local_chip_mem_start_address);
                 print_str(address_prefix, "...\r\n\r\n\r\n");
                 return;
                 break;
