6ba4ca32a35e57c703674547925b7d026beb8d0f
Yunhao Deng <IveanEx@outlook.com>
2025-04-20 22:25:41 +0200

    Ydeng/new reshape (#128)
    * Update reshape C prog and other automation experiment script
    
    * Update automated Makefile

--- target/sim/sw/device/apps/snax/snax-xdma-reshape/src/snax-xdma-reshape.c
+++ target/sim/sw/device/apps/snax/snax-xdma-reshape/src/snax-xdma-reshape.c
@@ -33,9 +33,9 @@ int main() {
     snrt_global_barrier();
 
     if (snrt_cluster_idx() == 0 && snrt_is_dm_core()) {
+        // The baseline 1 group: Copy by IDMA
         tcdm_in = (void *)tcdm_baseaddress + cluster_offset;
         tcdm_out = (void *)tcdm_baseaddress;
-        // The baseline group: Copy by IDMA
         char *src_addr[TOTAL_ITERATIONS_IDMA];
         char *dst_addr[TOTAL_ITERATIONS_IDMA];
         for (uint32_t i = 0; i < TOTAL_ITERATIONS_IDMA; i++) {
@@ -75,7 +75,7 @@ int main() {
         printf("The IDMA copy is finished in %d cycles\r\n",
                end_time - start_time);
 
-        // The XDMA group: Copy by XDMA
+        // The baseline 2 group: Copy by IDMA, then use XDMA as the acc to do
         // --------------------- Configure the Ext --------------------- //
 
         if (xdma_disable_dst_ext(0) != 0) {
@@ -90,6 +90,28 @@ int main() {
             printf("Error in disabling xdma reader extension 0\n");
             err++;
         }
+        // local reshape
+        uint8_t *tcdm_middle =
+            (void *)(tcdm_baseaddress +
+                     (matrix_size * sizeof(uint8_t) * 8 + 7) / 8);
+
+        xdma_memcpy_nd(tcdm_middle, tcdm_out, spatial_stride_src_xdma,
+                       spatial_stride_dst_xdma, temporal_dimension_src_xdma,
+                       temporal_strides_src_xdma, temporal_bounds_src_xdma,
+                       temporal_dimension_dst_xdma, temporal_strides_dst_xdma,
+                       temporal_bounds_dst_xdma, 0xFFFFFFFF, 0xFFFFFFFF,
+                       0xFFFFFFFF);
+        __asm__ volatile("csrr %0, mcycle;" : "=r"(start_time));
+        snrt_dma_start_1d(tcdm_middle, tcdm_in,
+                          matrix_size * sizeof(input_matrix[0]));
+        snrt_dma_wait_all();
+        int task_id = xdma_start();
+        xdma_local_wait(task_id);
+        __asm__ volatile("csrr %0, mcycle;" : "=r"(end_time));
+        printf("The IDMA copy + XDMA Reshape is finished in %d cycles\r\n",
+               end_time - start_time);
+
+        // The XDMA group: Copy by XDMA
 
         // --------------------- Configure the AGU --------------------- //
         xdma_memcpy_nd(tcdm_in, tcdm_out, spatial_stride_src_xdma,
@@ -100,7 +122,7 @@ int main() {
                        0xFFFFFFFF);
 
         __asm__ volatile("csrr %0, mcycle;" : "=r"(start_time));
-        int task_id = xdma_start();
+        task_id = xdma_start();
         xdma_remote_wait(task_id);
         __asm__ volatile("csrr %0, mcycle;" : "=r"(end_time));
         printf("The XDMA copy is finished in %d cycles\r\n",
--- target/sim_chip/automation/automated-xdma-reshape/start.py
+++ target/sim_chip/automation/automated-xdma-reshape/start.py
@@ -9,15 +9,13 @@ binary_path = "/users/micas/ydeng/Desktop/SNAX/HeMAiA/target/sim_chip/bin/occamy
 hemaia_root_path = "/users/micas/ydeng/Desktop/SNAX/HeMAiA"
 
 num_threads = 24
-# Ms = [32, 64, 96, 128, 192, 256, 384, 512]
-# Ns = [32, 64, 96, 128, 192, 256, 384, 512]
-# input_layouts = ["MNM8N32", "MNM8N16", "MNM8N8", "MN"]
-# output_layouts = ["MNM8N32", "MNM8N16", "MNM8N8", "MN"]
-
-Ms = [512]
-Ns = [512]
+sizes = [32, 64, 96, 128, 192, 256, 384, 512]
 input_layouts = ["MNM8N32", "MNM8N16", "MNM8N8", "MN"]
 output_layouts = ["MNM8N32", "MNM8N16", "MNM8N8", "MN"]
+
+# sizes = [64]
+# input_layouts = ["MN"]
+# output_layouts = ["MN"]
 
 
 # Load the existing params from params.hjson (so we only modify the needed keys)
@@ -29,7 +27,7 @@ with open(param_path, 'r') as f:
 result_csv_path = "result.csv"
 with open(result_csv_path, 'w') as result_csv:
     # Write the header row
-    result_csv.write("M,N,input_layout,output_layout,idma,xdma\n")
+    result_csv.write("M,N,input_layout,output_layout,idma,idma+xdma,xdma\n")
 
 # The function needs to be parallelized
 def process_configuration(folder_name, M, N, input_layout, output_layout):
@@ -40,14 +38,17 @@ def process_configuration(folder_name, M, N, input_layout, output_layout):
     
     subprocess.run(["./occamy_chip.vlt"], cwd=folder_name, check=True)
     with open(os.path.join(folder_name, "uart_0_0.log"), "r") as log_file:
-        baseline_cycle_line = next((ln for ln in log_file if "The IDMA copy is finished in" in ln), "")
-        baseline_cycle_val = baseline_cycle_line.split("in")[-1].split("cycles")[0].strip() if baseline_cycle_line else "N/A"
+        baseline_1_cycle_line = next((ln for ln in log_file if "The IDMA copy is finished in" in ln), "")
+        baseline_1_cycle_val = baseline_1_cycle_line.split("in")[-1].split("cycles")[0].strip() if baseline_1_cycle_line else "N/A"
+
+        baseline_2_cycle_line = next((ln for ln in log_file if "The IDMA copy + XDMA Reshape is finished in" in ln), "")
+        baseline_2_cycle_val = baseline_2_cycle_line.split("in")[-1].split("cycles")[0].strip() if baseline_2_cycle_line else "N/A"
 
         xdma_cycle_line = next((ln for ln in log_file if "The XDMA copy is finished in" in ln), "")
-        xdma_cycle_val = xdma_cycle_line.split("in")[-1].split("cycles")[0].strip() if baseline_cycle_line else "N/A"
+        xdma_cycle_val = xdma_cycle_line.split("in")[-1].split("cycles")[0].strip() if baseline_1_cycle_line else "N/A"
 
     with open(result_csv_path, "a") as result_file:
-        result_file.write(f"{M},{N},{input_layout},{output_layout},{baseline_cycle_val},{xdma_cycle_val}\n")
+        result_file.write(f"{M},{N},{input_layout},{output_layout},{baseline_1_cycle_val},{baseline_2_cycle_val},{xdma_cycle_val}\n")
 
     # 5. Delete the entire folder
     shutil.rmtree(folder_name)
@@ -56,34 +57,33 @@ def process_configuration(folder_name, M, N, input_layout, output_layout):
 
 with ThreadPoolExecutor(max_workers=num_threads) as executor:
     futures = []
-    for M in Ms:
-        for N in Ns:
-            for input_layout in input_layouts:
-                for output_layout in output_layouts:
-                    # 1. Prepa re folder name based on the configuration
-                    folder_name = f"M_{M}_N_{N}_IL_{input_layout}_OL_{output_layout}"
-                    os.makedirs(folder_name, exist_ok=True)
+    for Size in sizes:
+        for input_layout in input_layouts:
+            for output_layout in output_layouts:
+                # 1. Prepa re folder name based on the configuration
+                folder_name = f"M_{Size}_N_{Size}_IL_{input_layout}_OL_{output_layout}"
+                os.makedirs(folder_name, exist_ok=True)
 
-                    # 2. Modify the params.hjson values in place
-                    param_data["M"] = M
-                    param_data["N"] = N
-                    param_data["input_layout"] = input_layout
-                    param_data["output_layout"] = output_layout
+                # 2. Modify the params.hjson values in place
+                param_data["M"] = Size
+                param_data["N"] = Size
+                param_data["input_layout"] = input_layout
+                param_data["output_layout"] = output_layout
 
-                    with open(param_path, 'w') as f:
-                        hjson.dump(param_data, f)
+                with open(param_path, 'w') as f:
+                    hjson.dump(param_data, f)
 
-                    # 3b. Execute "make sw" and then "make apps" in the apps folder
-                    apps_path = os.path.join(hemaia_root_path, "target/sim_chip/apps")
-                    subprocess.run(["make", "sw", "-j"], cwd=hemaia_root_path, check=True)
-                    subprocess.run(["make", "apps"], cwd=apps_path, check=True)
+                # 3b. Execute "make sw" and then "make apps" in the apps folder
+                apps_path = os.path.join(hemaia_root_path, "target/sim_chip/apps")
+                subprocess.run(["make", "sw", "-j"], cwd=hemaia_root_path, check=True)
+                subprocess.run(["make", "apps"], cwd=apps_path, check=True)
 
-                    # 4. Copy the "snax-xdma-transpose" folder to the new folder
-                    source_dir = os.path.join(apps_path, "offload-snax-xdma-reshape")
-                    target_dir = os.path.join(folder_name, "app_chip_0_0")
-                    shutil.copytree(source_dir, target_dir)
+                # 4. Copy the "snax-xdma-transpose" folder to the new folder
+                source_dir = os.path.join(apps_path, "offload-snax-xdma-reshape")
+                target_dir = os.path.join(folder_name, "app_chip_0_0")
+                shutil.copytree(source_dir, target_dir)
 
-                    futures.append(executor.submit(process_configuration, folder_name, M, N, input_layout, output_layout))
+                futures.append(executor.submit(process_configuration, folder_name, Size, Size, input_layout, output_layout))
 
     # Wait for all threads to complete
     for future in futures:
