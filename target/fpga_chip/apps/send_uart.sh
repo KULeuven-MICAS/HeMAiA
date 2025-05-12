6fb1fb9a2104d62eb8298d149277f902726e9c1e
IveanEx <yunhao.deng@kuleuven.be>
2025-05-12 14:17:44 +0200

    Add Makefile and send_uart script for FPGA application management

---
+++ target/fpga_chip/apps/send_uart.sh
@@ -0,0 +1,20 @@
+# /bin/bash
+
+if [ "$#" -ne 0 ] && [ "$#" -ne 1 ];then
+	echo "Usage: send_uart [filename]"
+	exit 1
+else
+
+	if [ "$#" -ne 1 ];then
+		FILE=app.bin
+	else
+		FILE=$1
+	fi
+
+	stty -F /dev/ttyUSB10 cs8 500000 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon crtscts
+
+	echo -n 3 > /dev/ttyUSB10
+
+	sx -k "$FILE" < /dev/ttyUSB10 > /dev/ttyUSB10
+
+fi
