4f211d0643040ac9099eae8d78086055145b153d
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-06-13 17:00:43 +0200

    add gemmx compute code

--- target/sim/sw/device/apps/snax/snax-gemmx-dimc-cosim/src/gemmx-dimc-cosim_v2.c
+++ target/sim/sw/device/apps/snax/snax-gemmx-dimc-cosim/src/gemmx-dimc-cosim_v2.c
@@ -18,6 +18,8 @@
 #include "snax-gemmx-params.h"
 #include "snax-gemmx-lib.h"
 
+#include "data_gemmx.h"
+
 uint64_t * tcdm_1_start_addr; // starting address for GEMMX cluster
 uint64_t * tcdm_3_start_addr; // starting address for DIMC cluster
 
@@ -210,9 +212,59 @@ int main() {
     if (snrt_cluster_idx() == 1){
 
         if (snrt_is_compute_core()) {
-            printf("GEMMX cluster starts to check copied data\n");
+            printf("GEMMX cluster starts to configure GEMMX\n");
+            // Set Streamer configuration CSR for conv2d
+            set_gemmx_streamer_csr(
+                Aslstride0, Aslstride1, Atlbound0, Atlstride0, Atlbound1,
+                Atlstride1, Atlbound2, Atlstride2, Atlbound3, Atlstride3,
+                Atlbound4, Atlstride4, Atlbound5, Atlstride5,
+                set_addr_remap_index_A,
+
+                Bslstride0, Bslstride1, Btlbound0, Btlstride0, Btlbound1,
+                Btlstride1, Btlbound2, Btlstride2, set_addr_remap_index_B,
+
+                D8slstride0, D8slstride1, D8tlbound0, D8tlstride0, D8tlbound1,
+                D8tlstride1, D8tlbound2, D8tlstride2, set_addr_remap_index_D8,
+
+                Cslstride0, Cslstride1, Ctlbound0, Ctlstride0, Ctlbound1,
+                Ctlstride1, Ctlbound2, Ctlstride2, set_addr_remap_index_C,
+
+                D32slstride0, D32slstride1, D32tlbound0, D32tlstride0,
+                D32tlbound1, D32tlstride1, D32tlbound2, D32tlstride2,
+                set_addr_remap_index_D32,
+
+                delta_local_a, delta_local_b, delta_local_d8, delta_local_c,
+                delta_local_d32, bypassSIMD, transposed_A, transposed_B,
+                channel_en_C, broadcast_C);
+
+            // Set GEMMX configuration CSR
+            uint32_t subtraction_setting =
+                gen_subtraction_config(subtraction_a, subtraction_b);
+
+            uint32_t csr0 =
+                gen_csr0_config(input_zp_i, output_zp_i, max_int_i, min_int_i);
+            uint32_t csr1 = gen_csr1_config(double_round_i);
+
+            set_gemmx_csr(
+                gemmx_K, gemmx_N, gemmx_M, subtraction_setting, csr0, csr1,
+                shared_bitpacked_shift0, shared_bitpacked_shift1,
+                shared_multiplier0, shared_multiplier1, shared_multiplier2,
+                shared_multiplier3, shared_multiplier4, shared_multiplier5,
+                shared_multiplier6, shared_multiplier7, gemmx_M * gemmx_N, bypassSIMD);
+
+            // Set CSR to start Streamer for conv2d
+            set_gemmx_streamer_start();
+
+            // Set CSR to start GEMM
+            set_gemmx_start();
+
+            // Poll until Streamer and GEMM accelerator finish
+            wait_gemmx_and_streamer();
+            printf("GEMMX cluster finished the computation\n");
+
             // check the final result
-            
+            printf("GEMMX cluster starts to check copied data\n");
+
             for (int i = 0; i < 512; ++i) {
                 uint64_t value = dimc_tcdm_ptr_0[i];
 
