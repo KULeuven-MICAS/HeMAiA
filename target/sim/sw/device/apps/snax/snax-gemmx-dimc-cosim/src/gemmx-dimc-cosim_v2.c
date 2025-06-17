a5887082f06e63ce2d76342c969cd15840a09e35
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-06-17 12:42:29 +0200

    add dimc-gemmx compute co-sim and tiling

--- target/sim/sw/device/apps/snax/snax-gemmx-dimc-cosim/src/gemmx-dimc-cosim_v2.c
+++ target/sim/sw/device/apps/snax/snax-gemmx-dimc-cosim/src/gemmx-dimc-cosim_v2.c
@@ -18,6 +18,7 @@
 #include "snax-gemmx-params.h"
 #include "snax-gemmx-lib.h"
 
+#include "cfg_gemmx.h"
 #include "data_gemmx.h"
 
 uint64_t * tcdm_1_start_addr; // starting address for GEMMX cluster
@@ -31,7 +32,7 @@ int main() {
         // allocate memory for GEMMX cluster
         tcdm_1_start_addr = (uint64_t *)snrt_cluster_base_addrl();
     }
-    
+
     snrt_global_barrier();
 
     if (snrt_cluster_idx() == 3){
@@ -291,7 +292,30 @@ int main() {
 
     snrt_global_barrier();
 
-    if (snrt_cluster_idx() == 1){
+    if (snrt_cluster_idx() == 1) {
+        // allocate memory for GEMMX cluster
+        tcdm_1_start_addr = (uint8_t *)snrt_cluster_base_addrl();
+
+        // Prepare addresses in TCDM
+        int8_t *local_b;
+        int32_t *local_d32;
+
+        // Allocate space in TCDM
+        local_b = (int8_t *)(snrt_cluster_base_addrl() + delta_local_b);
+        local_d32 = (int32_t *)(snrt_cluster_base_addrl() + delta_local_d32);
+
+        if (snrt_is_dm_core()) {
+            snrt_dma_start_1d(
+                tcdm_1_start_addr, gold,
+                gemmx_M * gemmx_K * tileSize * meshRow * sizeof(int8_t));
+            snrt_dma_start_1d(
+                local_b, B_blocked_col_major_first_half,
+                gemmx_N * gemmx_K * tileSize * meshCol * sizeof(int8_t));
+            snrt_dma_wait_all();
+        }
+
+        // Wait for DMA to finish
+        snrt_cluster_hw_barrier();
 
         if (snrt_is_compute_core()) {
             printf("GEMMX cluster starts to configure GEMMX\n");
@@ -327,12 +351,14 @@ int main() {
                 gen_csr0_config(input_zp_i, output_zp_i, max_int_i, min_int_i);
             uint32_t csr1 = gen_csr1_config(double_round_i);
 
-            set_gemmx_csr(
-                gemmx_K, gemmx_N, gemmx_M, subtraction_setting, csr0, csr1,
-                shared_bitpacked_shift0, shared_bitpacked_shift1,
-                shared_multiplier0, shared_multiplier1, shared_multiplier2,
-                shared_multiplier3, shared_multiplier4, shared_multiplier5,
-                shared_multiplier6, shared_multiplier7, gemmx_M * gemmx_N, bypassSIMD);
+            set_gemmx_csr(gemmx_K, gemmx_N, gemmx_M, subtraction_setting, csr0,
+                          csr1, shared_bitpacked_shift0,
+                          shared_bitpacked_shift1, shared_multiplier0,
+                          shared_multiplier1, shared_multiplier2,
+                          shared_multiplier3, shared_multiplier4,
+                          shared_multiplier5, shared_multiplier6,
+                          shared_multiplier7, gemmx_M * gemmx_N, bypassSIMD);
+            printf("GEMMX cluster configuration finished\n");
 
             // Set CSR to start Streamer for conv2d
             set_gemmx_streamer_start();
@@ -345,25 +371,121 @@ int main() {
             printf("GEMMX cluster finished the computation\n");
 
             // check the final result
-            printf("GEMMX cluster starts to check copied data\n");
+            printf("GEMMX cluster starts to check the computed data\n");
 
-            for (int i = 0; i < 512; ++i) {
-                uint64_t value = tcdm_1_start_addr[i];
-
-                uint64_t index = i * 8;
-
-                // Split each uint64_t element into 8 uint8_t elements
-                for (int j = 0; j < 8; ++j) {
-                    uint8_t tmp_res = (uint8_t)((value >> (j * 8)) & 0xFF);
-                    // printf("%d ", tmp_res);
-                    if(tmp_res != gold[index + j]) {
-                        printf("MISMATCH at %d, res:%d, gold:%d\n", (index + j), tmp_res, gold[index + j]);
-                        err += 1;
-                    }
+            for (int i = 0; i < (gemmx_M * gemmx_N * meshRow * meshCol); i++) {
+                if (local_d32[i] != D_golden_blocked_row_major_first_half[i]) {
+                    err++;
+                    printf(
+                        "GEMMX cluster failed to verify the computed data at "
+                        "index %d, expected %d but got %d\n",
+                        i, D_golden_blocked_row_major_first_half[i], local_d32[i]);
                 }
-                printf("RESULTS MATCH WITH GOLDEN MODEL\n");
+            }
+
+            if (err == 0) {
+                printf(
+                    "GEMMX cluster verified the computed data successfully!\n");
+            } else {
+                printf(
+                    "GEMMX cluster failed to verify the computed data, err = "
+                    "%d\n",
+                    err);
             }
         }
+
+        // second_half
+
+        if (snrt_is_dm_core()) {
+            snrt_dma_start_1d(
+                tcdm_1_start_addr, gold,
+                gemmx_M * gemmx_K * tileSize * meshRow * sizeof(int8_t));
+            snrt_dma_start_1d(
+                local_b, B_blocked_col_major_second_half,
+                gemmx_N * gemmx_K * tileSize * meshCol * sizeof(int8_t));
+            snrt_dma_wait_all();
+        }
+
+        // Wait for DMA to finish
+        snrt_cluster_hw_barrier();
+
+        if (snrt_is_compute_core()) {
+            printf("GEMMX cluster starts to configure GEMMX\n");
+            // Set Streamer configuration CSR for conv2d
+            set_gemmx_streamer_csr(
+                Aslstride0, Aslstride1, Atlbound0, Atlstride0, Atlbound1,
+                Atlstride1, Atlbound2, Atlstride2, Atlbound3, Atlstride3,
+                Atlbound4, Atlstride4, Atlbound5, Atlstride5,
+                set_addr_remap_index_A,
+
+                Bslstride0, Bslstride1, Btlbound0, Btlstride0, Btlbound1,
+                Btlstride1, Btlbound2, Btlstride2, set_addr_remap_index_B,
+
+                D8slstride0, D8slstride1, D8tlbound0, D8tlstride0, D8tlbound1,
+                D8tlstride1, D8tlbound2, D8tlstride2, set_addr_remap_index_D8,
+
+                Cslstride0, Cslstride1, Ctlbound0, Ctlstride0, Ctlbound1,
+                Ctlstride1, Ctlbound2, Ctlstride2, set_addr_remap_index_C,
+
+                D32slstride0, D32slstride1, D32tlbound0, D32tlstride0,
+                D32tlbound1, D32tlstride1, D32tlbound2, D32tlstride2,
+                set_addr_remap_index_D32,
+
+                delta_local_a, delta_local_b, delta_local_d8, delta_local_c,
+                delta_local_d32, bypassSIMD, transposed_A, transposed_B,
+                channel_en_C, broadcast_C);
+
+            // Set GEMMX configuration CSR
+            uint32_t subtraction_setting =
+                gen_subtraction_config(subtraction_a, subtraction_b);
+
+            uint32_t csr0 =
+                gen_csr0_config(input_zp_i, output_zp_i, max_int_i, min_int_i);
+            uint32_t csr1 = gen_csr1_config(double_round_i);
+
+            set_gemmx_csr(gemmx_K, gemmx_N, gemmx_M, subtraction_setting, csr0,
+                          csr1, shared_bitpacked_shift0,
+                          shared_bitpacked_shift1, shared_multiplier0,
+                          shared_multiplier1, shared_multiplier2,
+                          shared_multiplier3, shared_multiplier4,
+                          shared_multiplier5, shared_multiplier6,
+                          shared_multiplier7, gemmx_M * gemmx_N, bypassSIMD);
+            printf("GEMMX cluster configuration finished\n");
+
+            // Set CSR to start Streamer for conv2d
+            set_gemmx_streamer_start();
+
+            // Set CSR to start GEMM
+            set_gemmx_start();
+
+            // Poll until Streamer and GEMM accelerator finish
+            wait_gemmx_and_streamer();
+            printf("GEMMX cluster finished the computation\n");
+
+            // check the final result
+            printf("GEMMX cluster starts to check the computed data\n");
+
+            for (int i = 0; i < (gemmx_M * gemmx_N * meshRow * meshCol); i++) {
+                if (local_d32[i] != D_golden_blocked_row_major_first_half[i]) {
+                    err++;
+                    printf(
+                        "GEMMX cluster failed to verify the computed data at "
+                        "index %d, expected %d but got %d\n",
+                        i, D_golden_blocked_row_major_second_half[i], local_d32[i]);
+                }
+            }
+
+            if (err == 0) {
+                printf(
+                    "GEMMX cluster verified the computed data successfully!\n");
+            } else {
+                printf(
+                    "GEMMX cluster failed to verify the computed data, err = "
+                    "%d\n",
+                    err);
+            }
+        }
+
     }
 
     snrt_global_barrier();
