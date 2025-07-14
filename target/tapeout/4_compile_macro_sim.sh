fd70723367404d7089e0828af74b900b8be8c678
IveanEx <yunhao.deng@kuleuven.be>
2025-07-14 15:05:37 +0200

    Change common_cells to our local impl; update two synthesis script

--- target/tapeout/4_compile_macro_sim.sh
+++ target/tapeout/4_compile_macro_sim.sh
@@ -2,5 +2,4 @@
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cfg_name="hemaia_tapeout.hjson"
 
-make -C "$script_dir/../.." hemaia_system_vsim_preparation SIM_WITH_TSMC16_MACRO=1
 make -C "$script_dir/../.." hemaia_system_vsim
