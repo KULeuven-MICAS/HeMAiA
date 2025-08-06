a3e4c772cb763f210ed742b0494e9aa28c0f9a23
Yunhao Deng <IveanEx@outlook.com>
2025-08-06 20:42:49 +0200

    Update d2d_link location (#174)

--- target/tapeout/1_init_outside_docker.sh
+++ target/tapeout/1_init_outside_docker.sh
@@ -5,7 +5,7 @@ if [ ! -d "$script_dir/../../hw/hemaia/tech_cells_tsmc16" ]; then
     git clone https://github.com/IveanEx/tech_cells_tsmc16.git "$script_dir/../../hw/hemaia/tech_cells_tsmc16"
 fi
 if [ ! -d "$script_dir/../../hw/hemaia/hemaia_d2d_link" ]; then
-    git clone https://github.com/KULeuven-MICAS/hemaia_d2d_link.git "$script_dir/../../hw/hemaia/hemaia_d2d_link"
+    git clone https://github.com/IveanEx/hemaia_d2d_link.git "$script_dir/../../hw/hemaia/hemaia_d2d_link"
 fi
 if [ ! -d "$script_dir/dc_work_hemaia" ]; then
     git clone https://github.com/IveanEx/dc_work_hemaia.git "$script_dir/dc_work_hemaia"
