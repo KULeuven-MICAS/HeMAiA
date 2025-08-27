5d6ccd90ca29dc76d9cc143b8d30aca2431969d9
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-08-27 15:33:20 +0200

    add batch 32

--- target/sim/sw/device/apps/snax/snax-gemmx-resnet8/src/snax-gemmx-resnet8.c
+++ target/sim/sw/device/apps/snax/snax-gemmx-resnet8/src/snax-gemmx-resnet8.c
@@ -58,8 +58,28 @@ int main() {
             gen_csr0_config(input_zp_i, output_zp_i, max_int_i, min_int_i);
         uint32_t csr1 = gen_csr1_config(double_round_i);
 
-        // Transfer data from L3 to L1
-        // Using DMA only
+        // Obtain the start address of the TCDM memory
+        uint32_t tcdm_baseaddress = snrt_cluster_base_addrl();
+        // Put the input at the starting of tcdm
+        uint8_t *tcdm_in = (uint8_t *)tcdm_baseaddress;
+        // Put the output at the middle of tcdm
+        uint8_t *tcdm_out = (uint8_t *)(tcdm_baseaddress + delta_local_out);
+
+        // The xdma core is the last compute core in the cluster
+        uint32_t sstride_src[1] = {0};
+        uint32_t sstride_dst[1] = {0};
+        uint32_t tstride_src[5] = {0};
+        uint32_t tbound_src[5] = {0};
+        uint32_t tstride_dst[3] = {0};
+        uint32_t tbound_dst[3] = {0};
+
+        // ---------------------------------------------------------------
+        // ---------------------------------------------------------------
+        // ---------------------------------------------------------------
+        // Init conv and maxpool
+        // ---------------------------------------------------------------
+        // ---------------------------------------------------------------
+        // ---------------------------------------------------------------
         if (snrt_is_dm_core()) {
             snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_DMA_BUSY,
                                     snrt_hartid());
@@ -174,51 +194,6 @@ int main() {
 
             printf("[CONV]-GeMM performance counter: %d \r\n",
                    performance_counter);
-
-            int if_inifinit_loop = 0;
-            while (0) {
-                // Set CSR to start Streamer for conv2d
-                set_gemmx_streamer_start();
-
-                // Set CSR to start GEMM
-                set_gemmx_start();
-
-                // Poll until Streamer and GEMM accelerator finish
-                wait_gemmx_and_streamer();
-            }
-
-            // check the result of the implicit im2col convolution
-            // if (interleaved_address == 1) {
-            //     if (!bypassSIMD) {
-            //         err +=
-            //             check_gemmx_result_D8(local_d8, D8, Batch, M, N,
-            //             false);
-            //     } else {
-            //         err += check_gemmx_result_D32(local_d32, D32, Batch, M,
-            //         N,
-            //                                       false);
-            //     }
-            // } else {
-            //     if (!bypassSIMD) {
-            //         err += check_gemmx_result_D8(local_d8_dma, D8, Batch, M,
-            //         N,
-            //                                      true);
-            //     } else {
-            //         err += check_gemmx_result_D32(local_d32_dma, D32, Batch,
-            //         M,
-            //                                       N, true);
-            //     }
-            // }
-
-            // printf("SNAX GEMM Conv2d: %s, Error: %d . bypassSIMD = %d .\r\n",
-            //        err ? "FAIL" : "PASS", err, bypassSIMD);
-            // if (err == 0) {
-            //     write_csr_obs(0x00f);
-            //     printf("P \r\n");
-            // } else {
-            //     write_csr_obs(0x00e);
-            //     printf("F: %d \r\n", err);
-            // }
         };
         snrt_cluster_hw_barrier();
 
@@ -230,24 +205,13 @@ int main() {
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
 
-        // Obtain the start address of the TCDM memory
-        uint32_t dma_load_input_start;
-        uint32_t dma_load_input_end;
-        uint32_t tcdm_baseaddress = snrt_cluster_base_addrl();
         // Put the input at the starting of tcdm
-        uint8_t *tcdm_in = (uint8_t *)tcdm_baseaddress;
+        tcdm_in = (uint8_t *)(tcdm_baseaddress + delta_local_d8);
         // Put the output at the middle of tcdm
-        uint8_t *tcdm_out = (uint8_t *)(tcdm_baseaddress + delta_local_out);
+        // off-set to after the result from conv
+        tcdm_out = (uint8_t *)(tcdm_baseaddress + delta_local_d8 + 64 * 7 * 7);
 
         if (snrt_is_dm_core()) {
-            // The xdma core is the last compute core in the cluster
-            uint32_t sstride_src[1] = {0};
-            uint32_t sstride_dst[1] = {0};
-            uint32_t tstride_src[5] = {0};
-            uint32_t tbound_src[5] = {0};
-            uint32_t tstride_dst[3] = {0};
-            uint32_t tbound_dst[3] = {0};
-
             // Load the CFG from data.h
             sstride_src[0] = spatialStride1_in;
             sstride_dst[0] = spatialStride1_out;
@@ -267,10 +231,6 @@ int main() {
             tbound_dst[0] = tempLoop0_out;
             tbound_dst[1] = tempLoop1_out;
             tbound_dst[2] = tempLoop2_out;
-
-            // // First we need to transfer the input data from L3->TCDM
-            // snrt_dma_start_1d(tcdm_in, DataIn, input_data_len *
-            // sizeof(int8_t)); snrt_dma_wait_all();
 
             // --------------------- Configure the Ext --------------------- //
 
@@ -310,24 +270,148 @@ int main() {
 
             printf("[MAXPOOL]-XDMA cycle from mcycle: %d \r\n",
                    end_cycle - start_cycle);
-
-            // --------------------- Checking the Results ---------------------
-            // //
-            // for (int i = 0; i < output_data_len; i++) {
-            //     if ((int8_t)tcdm_out[i] != C_golden[i]) {
-            //         printf("The maxpool is incorrect!\r\n");
-            //         printf("tcdm_out[%d]=%d, C_golden[%d]=%d", i,
-            //                (int8_t)tcdm_out[i], i, C_golden[i]);
-            //     }
-            // }
-            // printf("Checking is done. All values are right\r\n");
         }
         snrt_cluster_hw_barrier();
 
+        for (int i = 1; i < 32; i++) {
+            // load the same data
+            if (snrt_is_dm_core()) {
+                snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_DMA_BUSY,
+                                        snrt_hartid());
+                if (interleaved_address == 1) {
+                    start_cycle = snrt_mcycle();
+                    snrt_dma_start_1d(local_a, A,
+                                      Nbatch * (H + 2 * pad_h) *
+                                          (W + 2 * pad_w) * Cin *
+                                          sizeof(int8_t));
+                    snrt_dma_start_1d(local_b, B,
+                                      Cout * Kh * Kw * Cin * sizeof(int8_t));
+                } else {
+                    start_cycle = snrt_mcycle();
+                    snrt_dma_start_2d(
+                        local_a_dma, A, 64 * sizeof(int8_t), 256, 64,
+                        Nbatch * (H + 2 * pad_h) * (W + 2 * pad_w) * Cin / 64);
+                    snrt_dma_start_2d(local_b_dma, B, 64 * sizeof(int8_t), 256,
+                                      64, Cout * Kh * Kw * Cin / 64);
+                }
+                snrt_dma_wait_all();
+                end_cycle = snrt_mcycle();
+                printf(
+                    "[CONV-%d]-DMA transfer cycle from DMA hardware counter %d  "
+                    "for A "
+                    "and B\r\n", i, snrt_get_perf_counter(SNRT_PERF_CNT0));
+                snrt_reset_perf_counter(SNRT_PERF_CNT0);
+                printf(
+                    "[CONV-%d]-DMA transfer cycle from mcycle: %d cycles for A "
+                    "and B "
+                    "\r\n", i, end_cycle - start_cycle);
+            }
+
+            // Wait for DMA to finish
+            snrt_cluster_hw_barrier();
+            if (snrt_is_dm_core()) {
+                snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_DMA_BUSY,
+                                        snrt_hartid());
+                if (interleaved_address == 1) {
+                    start_cycle = snrt_mcycle();
+                    snrt_dma_start_1d(
+                        local_c, C,
+                        M * N * meshRow * meshCol * sizeof(int32_t));
+                } else {
+                    start_cycle = snrt_mcycle();
+                    snrt_dma_start_2d(local_c_dma, C, 16 * sizeof(int32_t), 256,
+                                      16 * sizeof(int32_t),
+                                      M * N * meshRow * meshCol / 16);
+                }
+                snrt_dma_wait_all();
+                end_cycle = snrt_mcycle();
+                printf(
+                    "[CONV-%d]-DMA transfer cycle from DMA hardware counter %d "
+                    "\r\n", i,
+                    snrt_get_perf_counter(SNRT_PERF_CNT0));
+                snrt_reset_perf_counter(SNRT_PERF_CNT0);
+                printf(
+                    "[CONV-%d]-DMA transfer cycle from mcycle: %d cycles for C "
+                    "\r\n", i, end_cycle - start_cycle);
+            }
+
+            snrt_cluster_hw_barrier();
+
+            if (snrt_cluster_core_idx() == 0) {
+                // Set CSR to start Streamer for conv2d
+                start_cycle = snrt_mcycle();
+                set_gemmx_streamer_start();
+
+                // Set CSR to start GEMM
+                set_gemmx_start();
+
+                // Poll until Streamer and GEMM accelerator finish
+                wait_gemmx_and_streamer();
+                end_cycle = snrt_mcycle();
+
+                printf("[CONV-%d]-Compute total cycles from mcycle: %d \r\n",
+                       i, end_cycle - start_cycle);
+
+                performance_counter = read_gemmx_streamer_perf_counter();
+
+                printf("[CONV-%d]-GeMM performance counter: %d \r\n",
+                       i, performance_counter);
+            };
+            snrt_cluster_hw_barrier();
+
+            // off-set to after the result from conv
+            tcdm_out = (uint8_t *)(tcdm_baseaddress + delta_local_d8 +
+                                   64 * 7 * 7 + i * 64);
+
+            if (snrt_is_dm_core()) {
+                // --------------------- Configure the Ext ---------------------
+                // //
+
+                if (xdma_disable_dst_ext(0) != 0) {
+                    printf("Error in disabling xdma extension 0\r\n");
+                    err++;
+                } else {
+                    printf("The xdma extension 0 is disabled\r\n");
+                }
+
+                uint32_t ext_param_maxpool_size[1] = {reduceLen};
+                if (xdma_enable_dst_ext(1, ext_param_maxpool_size) != 0) {
+                    printf("Error in enabling xdma extension 1\r\n");
+                    err++;
+                } else {
+                    printf("The xdma extension 1 is enabled\r\n");
+                }
+
+                if (xdma_disable_dst_ext(2) != 0) {
+                    printf("Error in disabling xdma extension 2\r\n");
+                    err++;
+                } else {
+                    printf("The xdma extension 2 is disabled\r\n");
+                }
+
+                // --------------------- Configure the AGU ---------------------
+                // //
+
+                xdma_memcpy_nd(tcdm_in, tcdm_out, sstride_src, sstride_dst, 5,
+                               tstride_src, tbound_src, 3, tstride_dst,
+                               tbound_dst, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF);
+                start_cycle = snrt_mcycle();
+
+                int task_id = xdma_start();
+                xdma_wait(task_id);
+
+                end_cycle = snrt_mcycle();
+
+                printf("[MAXPOOL-%d]-XDMA cycle from mcycle: %d \r\n",
+                       i, end_cycle - start_cycle);
+            }
+            snrt_cluster_hw_barrier();
+        }
+
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
-        // FC
+        // Batched FC
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
@@ -347,9 +431,6 @@ int main() {
         // Transfer data from L3 to L1
         // Using DMA only
         if (snrt_is_dm_core()) {
-            // snrt_dma_start_1d(local_a_fc, A_fc,
-            //                   M_fc * K_fc * meshRow_fc * tileSize_fc *
-            //                   sizeof(int8_t));
             snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_DMA_BUSY,
                                     snrt_hartid());
             snrt_dma_start_1d(
@@ -408,7 +489,7 @@ int main() {
                 D32tlstride0_fc, D32tlbound1_fc, D32tlstride1_fc,
                 D32tlbound2_fc, D32tlstride2_fc, set_addr_remap_index_D32_fc,
 
-                delta_local_a_fc, delta_local_b_fc, delta_local_d8_fc,
+                delta_local_d8 + 64 * 7 * 7 + 32 * 64, delta_local_b_fc, delta_local_d8_fc,
                 delta_local_c_fc, delta_local_d32_fc, bypassSIMD_fc,
                 transposed_A_fc, transposed_B_fc, channel_en_C_fc,
                 broadcast_C_fc);
@@ -448,28 +529,6 @@ int main() {
                 // Poll until Streamer and GEMM accelerator finish
                 wait_gemmx_and_streamer();
             }
-
-            // printf("Computation finished \r\n");
-            // check the result of the implicit im2col convolution
-            // if (!bypassSIMD_fc) {
-            //     err += check_gemmx_result_D8(local_d8_fc, D8_fc, Batch_fc,
-            //     M_fc, N_fc, false);
-            // } else {
-            //     err +=
-            //         check_gemmx_result_D32(local_d32_fc, D32_fc, Batch_fc,
-            //         M_fc, N_fc, false);
-            // }
-
-            // printf("SNAX GEMM Matmul: %s, Error: %d . bypassSIMD = %d .\r\n",
-            //    err ? "FAIL" : "PASS", err, bypassSIMD);
-
-            if (err == 0) {
-                write_csr_obs(0x00f);
-                printf("P \r\n");
-            } else {
-                write_csr_obs(0x00e);
-                printf("F: %d \r\n", err);
-            }
         };
 
         snrt_cluster_hw_barrier();
@@ -477,7 +536,7 @@ int main() {
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
-        // Data Reshuffler
+        // Batched Data Reshuffler
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
         // ---------------------------------------------------------------
@@ -485,7 +544,8 @@ int main() {
         // Put the input at the starting of tcdm
         uint8_t *tcdm_in_dr = (uint8_t *)tcdm_baseaddress + delta_local_d8_fc;
         // Put the output at the middle of tcdm
-        uint8_t *tcdm_out_dr = (uint8_t *)(tcdm_baseaddress + delta_local_out_dr);
+        uint8_t *tcdm_out_dr =
+            (uint8_t *)(tcdm_baseaddress + delta_local_out_dr);
 
         if (snrt_is_dm_core()) {
             // The xdma core is the last compute core in the cluster
@@ -545,9 +605,10 @@ int main() {
 
             // --------------------- Configure the AGU --------------------- //
 
-            xdma_memcpy_nd(tcdm_in_dr, tcdm_out_dr, sstride_src_dr, sstride_dst_dr, 5,
-                           tstride_src_dr, tbound_src_dr, 3, tstride_dst_dr, tbound_dst_dr,
-                           0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF);
+            xdma_memcpy_nd(tcdm_in_dr, tcdm_out_dr, sstride_src_dr,
+                           sstride_dst_dr, 5, tstride_src_dr, tbound_src_dr, 3,
+                           tstride_dst_dr, tbound_dst_dr, 0xFFFFFFFF,
+                           0xFFFFFFFF, 0xFFFFFFFF);
             start_cycle = snrt_mcycle();
 
             int task_id = xdma_start();
@@ -557,7 +618,6 @@ int main() {
 
             printf("[Data Reshuffler]-XDMA cycle from mcycle: %d \r\n",
                    end_cycle - start_cycle);
-
         }
 
         snrt_cluster_hw_barrier();
