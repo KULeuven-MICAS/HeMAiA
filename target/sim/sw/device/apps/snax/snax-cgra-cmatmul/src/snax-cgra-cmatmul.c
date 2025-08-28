6b4a7544e5179ab2ace3f25682763307045ddb20
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-08-28 21:12:47 +0200

    add dma perf counter

--- target/sim/sw/device/apps/snax/snax-cgra-cmatmul/src/snax-cgra-cmatmul.c
+++ target/sim/sw/device/apps/snax/snax-cgra-cmatmul/src/snax-cgra-cmatmul.c
@@ -26,6 +26,8 @@ int main() {
 
 		// Using DMA only
 		if (snrt_is_dm_core()) {
+            snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_DMA_BUSY,
+                                    snrt_hartid());
 			local_config_data = (int32_t *)(snrt_l1_next() + delta_config_lut);
 			snrt_dma_start_1d(local_config_data, CONFIG_LUT,
 							CONFIG_SIZE_LUT * sizeof(uint32_t));
@@ -57,6 +59,10 @@ int main() {
 			snrt_dma_start_1d(local_config_data, COMP_DATA,
 							COMP_DATA_SIZE * sizeof(uint32_t));
 			snrt_dma_wait_all();
+            printf(
+                "DMA transfer cycle from DMA hardware counter %d \r\n",
+                snrt_get_perf_counter(SNRT_PERF_CNT0));
+            snrt_reset_perf_counter(SNRT_PERF_CNT0);
 		}
 		snrt_cluster_hw_barrier();
 
@@ -65,35 +71,36 @@ int main() {
 			printf ("hello world!\r\n\r\n");
 			uint32_t mcycle_timestamps[7];
 
-			// launch_cgra_0(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
+			// // launch_cgra_0(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
 			
-			// launch_cgra_0_compact(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
+			// // launch_cgra_0_compact(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
 			
-			launch_cgra_0_config(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
-			launch_cgra_0_go(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
-
-			// cgra_hw_barrier(10, 1e5, 1, 1);
-			cgra_hw_barrier_fast(10, 1e5);
-
-			cgra_hw_profiler();
-
-			// fast run again, without re-load cfg
-			// launch_cgra_0_relaunch(mcycle_timestamps);
+			// launch_cgra_0_config(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
+			// launch_cgra_0_go(delta_config_data, delta_comp_data, delta_store_data, mcycle_timestamps);
 
 			// // cgra_hw_barrier(10, 1e5, 1, 1);
 			// cgra_hw_barrier_fast(10, 1e5);
 
 			// cgra_hw_profiler();
 
-			printf("mcycle cgra_init = %d\r\n", mcycle_timestamps[1] - mcycle_timestamps[0]);
-			printf("mcycle cgra_config_prep = %d\r\n", mcycle_timestamps[2] - mcycle_timestamps[1]);
-			printf("mcycle cgra_data_prep = %d\r\n", mcycle_timestamps[3] - mcycle_timestamps[2]);
-			printf("mcycle cgra_launch_init = %d\r\n", mcycle_timestamps[4] - mcycle_timestamps[3]);
-			printf("mcycle cgra_relaunch_init = %d\r\n", mcycle_timestamps[5] - mcycle_timestamps[4]);
-			printf("mcycle cgra_relaunch_done = %d\r\n", mcycle_timestamps[6] - mcycle_timestamps[5]);
-			printf("mcycle cgra_relaunch_done = %d\r\n", mcycle_timestamps[6] - mcycle_timestamps[5]);
+			// // fast run again, without re-load cfg
+			// // launch_cgra_0_relaunch(mcycle_timestamps);
+
+			// // // cgra_hw_barrier(10, 1e5, 1, 1);
+			// // cgra_hw_barrier_fast(10, 1e5);
+
+			// // cgra_hw_profiler();
+
+			// printf("mcycle cgra_init = %d\r\n", mcycle_timestamps[1] - mcycle_timestamps[0]);
+			// printf("mcycle cgra_config_prep = %d\r\n", mcycle_timestamps[2] - mcycle_timestamps[1]);
+			// printf("mcycle cgra_data_prep = %d\r\n", mcycle_timestamps[3] - mcycle_timestamps[2]);
+			// printf("mcycle cgra_launch_init = %d\r\n", mcycle_timestamps[4] - mcycle_timestamps[3]);
+			// printf("mcycle cgra_relaunch_init = %d\r\n", mcycle_timestamps[5] - mcycle_timestamps[4]);
+			// printf("mcycle cgra_relaunch_done = %d\r\n", mcycle_timestamps[6] - mcycle_timestamps[5]);
+			// printf("mcycle cgra_relaunch_done = %d\r\n", mcycle_timestamps[6] - mcycle_timestamps[5]);
 
 			return 0;
-		} else return 0;
+		} 
+		else return 0;
 	} else return 0;
 }
