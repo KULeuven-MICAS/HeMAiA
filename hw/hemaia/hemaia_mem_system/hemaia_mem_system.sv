5fe458c3b183c953b24d295879c2a07578185632
Konste11ation <fanchen.kong@outlook.be>
2025-08-31 13:29:30 +0200

    bug fix

--- hw/hemaia/hemaia_mem_system/hemaia_mem_system.sv
+++ hw/hemaia/hemaia_mem_system/hemaia_mem_system.sv
@@ -25,9 +25,6 @@ module hemaia_mem_system #(
     parameter type axi_narrow_slave_rsp_t = logic,
     // XMDA Defines
     parameter int unsigned ClusterAddressSpace = 48'h400000,
-
-    parameter int unsigned ClusterAddressSpace = 48'h400000,
-
     parameter int unsigned MemBaseAddr = 32'h80000000,
     parameter int unsigned MemBankNum = 32,
     parameter int unsigned MemSize = 32'h100000
