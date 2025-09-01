918ea7fded18a91447080d75d213afb40a761a5e
Konste11ation <fanchen.kong@outlook.be>
2025-09-01 14:50:37 +0200

    clean files

--- target/sw/device/apps/snax/snax-dummy-offload/src/snax-dummy-offload.c
+++ target/sw/device/apps/snax/snax-dummy-offload/src/snax-dummy-offload.c
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 #include "snrt.h"
-// #include "snax-dummy-offload.h"
 int main(){
     // First we need to write the kernel tab to the soc ctrl
     // We use the first cluster to do this
