28889086ea3c27dcfa2dd92eb31e2e7266d8e304
RobinGeens <robin.geens@kuleuven.be>
2025-12-02 21:28:47 +0100

    build succesful

--- target/sim/sw/device/snax/common.mk
+++ target/sim/sw/device/snax/common.mk
@@ -6,13 +6,11 @@ include $(MK_DIR)/../toolchain.mk
 ###############
 
 # Fixed paths in repository tree
-ROOT        = $(abspath $(MK_DIR)/../../../../../)
 SNITCH_ROOT = $(shell bender path snitch_cluster)
-APPSDIR     = $(abspath $(MK_DIR))
-RUNTIME_DIR = $(ROOT)/target/sim/sw/device/runtime
+RUNTIME_DIR = $(abspath $(MK_DIR)/../runtime)
 SNRT_DIR    = $(SNITCH_ROOT)/sw/snRuntime
-SW_DIR      = $(ROOT)/target/sim/sw/
-MATH_DIR    = $(ROOT)/target/sim/sw/device/math
+SW_DIR      = $(abspath $(MK_DIR)/../..)
+MATH_DIR    = $(abspath $(MK_DIR)/../math)
 
 # Paths relative to the app including this Makefile
 BUILDDIR    = $(abspath build)
@@ -23,25 +21,18 @@ SRC_DIR     = $(abspath src)
 ###################
 
 # Dependencies
-
-# occamy runtime
 INCDIRS += $(RUNTIME_DIR)/src
-# snitch runtime
 INCDIRS += $(SNRT_DIR)/api
 INCDIRS += $(SNRT_DIR)/src
-# riscv-opcodes
 INCDIRS += $(SNRT_DIR)/vendor/riscv-opcodes
-# math lib
+INCDIRS += $(SW_DIR)/shared/platform/generated
+INCDIRS += $(SW_DIR)/shared/runtime
 INCDIRS += $(SNRT_DIR)/../math/arch/riscv64/
 INCDIRS += $(SNRT_DIR)/../math/arch/generic
 INCDIRS += $(SNRT_DIR)/../math/src/include
 INCDIRS += $(SNRT_DIR)/../math/src/internal
 INCDIRS += $(SNRT_DIR)/../math/include/bits
 INCDIRS += $(SNRT_DIR)/../math/include
-# occamy platform shared
-INCDIRS += $(SW_DIR)/shared/platform/generated
-# INCDIRS += $(SW_DIR)/shared/platform
-INCDIRS += $(SW_DIR)/shared/runtime
 
 SNRT_LIB_DIR  = $(abspath $(RUNTIME_DIR)/build/)
 SNRT_LIB_NAME = snRuntime
