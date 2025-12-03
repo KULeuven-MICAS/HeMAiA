c9974546f9f2d93f98482d5cf4c314c8f2bc3b21
Konste11ation <fanchen.kong@outlook.be>
2025-12-03 18:55:47 +0100

    Update the hemaia start

--- target/sw/device/runtime/src/hemaia_start.c
+++ target/sw/device/runtime/src/hemaia_start.c
@@ -25,33 +25,40 @@
 
 #ifdef SNRT_INIT_TLS
 static inline void snrt_init_tls() {
-    // We only need to copy the tdata to the tcdm memory
     extern volatile uint32_t __tdata_start, __tdata_end;
-    uint8_t* tdata_src_l3 = (uint8_t*)&__tdata_start;
-    uint8_t* tdata_end_l3 = (uint8_t*)&__tdata_end;
-    uint32_t size;
+    extern volatile uint32_t __tbss_start, __tbss_end;
+
+    size_t size;
     volatile uint32_t tls_ptr;
+
+    // To avoid contentions in main memory, and take advantage of the
+    // bandwidth of the DMA, the DM core initializes the TLS section
+    // for every core in a cluster.
     if (snrt_is_dm_core()) {
         size = (size_t)(&__tdata_end) - (size_t)(&__tdata_start);
-        // in start.S see "snrt.crt0.init_tls"
-        // that the tp register is used to store the TLS pointer
+
+        // First initialize the DM core's .tdata section from main memory
         asm volatile("mv %0, tp" : "=r"(tls_ptr) : :);
-        uint8_t* tls_dst = (uint8_t*)tls_ptr;
-        for (size_t i = 0; i < size; i++) {
-            tls_dst[i] = tdata_src_l3[i];
-        }
+        snrt_dma_start_1d((void*)tls_ptr, (void*)(&__tdata_start), size);
+
         // Then initialize all other cores' .tdata sections from the DM
         // core's. The offset between the TLS section of successive cores
         // is defined in start.S
         size_t tls_offset = (1 << SNRT_LOG2_STACK_SIZE) + 8;
-        // Since the dm core is at the bottrom of the cluster, we can just adding the offset to the dm's tls pointer to get the other cores' tls pointer
         for (int i = 1; i < snrt_cluster_core_num(); i++) {
-            uint8_t* dst = (uint8_t*)(tls_ptr + i * tls_offset);
-            for (size_t j = 0; j < size; j++) {
-                dst[j] = tls_dst[j];
-            }
+            snrt_dma_start_1d((void*)(tls_ptr + i * tls_offset), (void*)tls_ptr,
+                              size);
+        }
+
+        // Initialize all cores' .tbss sections
+        tls_ptr += size;
+        size = (size_t)(&__tbss_end) - (size_t)(&__tbss_start);
+        for (int i = 0; i < snrt_cluster_core_num(); i++) {
+            snrt_dma_start_1d((void*)(tls_ptr + i * tls_offset),
+                              (void*)(snrt_zero_memory_ptr()), size);
         }
     }
+
     snrt_cluster_hw_barrier();
 }
 #endif
@@ -68,38 +75,31 @@ static inline uint32_t snrt_cls_base_addr() {
     extern volatile uint32_t __cbss_start, __cbss_end;
     uint32_t cdata_size = ((uint32_t)&__cdata_end) - ((uint32_t)&__cdata_start);
     uint32_t cbss_size = ((uint32_t)&__cbss_end) - ((uint32_t)&__cbss_start);
-    // uint32_t l1_end_addr = SNRT_TCDM_START_ADDR +
-    //                        snrt_cluster_idx() * SNRT_CLUSTER_OFFSET +
-    //                        SNRT_TCDM_SIZE;
     uint32_t l1_end_addr = snrt_cluster_base_addrl() + SNRT_TCDM_SIZE;
     return l1_end_addr - cdata_size - cbss_size;
 }
-
-static inline void snrt_init_cls(){
+#endif
+#ifdef SNRT_INIT_CLS
+static inline void snrt_init_cls() {
     extern volatile uint32_t __cdata_start, __cdata_end;
     extern volatile uint32_t __cbss_start, __cbss_end;
+
     _cls_ptr = (cls_t*)snrt_cls_base_addr();
-    if(snrt_is_dm_core()){
-        volatile uint8_t* tcdm_base = (volatile uint8_t*)snrt_cls_base_addr();
-        uint32_t size;
 
-        volatile uint8_t* cdata_src = (volatile uint8_t*)&__cdata_start;
-        volatile uint8_t* cdata_end = (volatile uint8_t*)&__cdata_end;
-        size = cdata_end - cdata_src;
+    // Only one core per cluster has to do this
+    if (snrt_is_dm_core()) {
+        void* ptr = (void*)snrt_cls_base_addr();
+        size_t size;
 
-        for (uint32_t i = 0; i < size; i++) {
-            tcdm_base[i] = cdata_src[i];
-        }
-        // if(snrt_cluster_idx() == 0){
-        //     printf("[Cluster 0]DM core initialized CLS section\n");
-        //     printf("[Cluster 0]DM core CLS addr 0x%x\n", _cls_ptr);
-        // }
-        // if(snrt_cluster_idx() == 1){
-        //     printf("[Cluster 1]DM core initialized CLS section\n");
-        //     printf("[Cluster 1]DM core TLS addr 0x%x\n", _cls_ptr);
-        // }
+        // Copy cdata section to base of the TCDM
+        size = (size_t)(&__cdata_end) - (size_t)(&__cdata_start);
+        snrt_dma_start_1d(ptr, (void*)(&__cdata_start), size);
+
+        // Clear cbss section
+        ptr = (void*)((uint32_t)ptr + size);
+        size = (size_t)(&__cbss_end) - (size_t)(&__cbss_start);
+        snrt_dma_start_1d(ptr, (void*)(WIDE_ZERO_MEM_BASE_ADDR), size);
     }
-    snrt_cluster_hw_barrier();
 }
 #endif
 
