5497c3c290a292ffd8d53425f70f368ac70615a7
Konste11ation <fanchen.kong@outlook.be>
2026-01-29 20:29:01 +0100

    update the gemm_stacked

--- target/sw/host/apps/offload_bingo_sw/single_chip/workloads/gemm_stacked/offload_bingo_sw.h
+++ target/sw/host/apps/offload_bingo_sw/single_chip/workloads/gemm_stacked/offload_bingo_sw.h
@@ -39,9 +39,10 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     // 3. Register the tasks
     // 4. Set the task dependency
     // 5. Set the assigned chiplet id and cluster id
-    uint8_t current_chip_id = get_current_chip_id();
+
     // only test chip 0 for intra-chiplet gemm
-    uint8_t task_chip_id = 0;
+    uint8_t assigned_chip_id = 0;
+    uint8_t assigned_cluster_id = 0;
 
     // 1 Get the needed kernel function address by the kernel name
     check_kernel_tab_ready();
@@ -50,11 +51,10 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     uint32_t __snax_kernel_xdma_1d_copy_func_addr = get_device_function("__snax_kernel_xdma_1d_copy");
     uint32_t __snax_kernel_idma_1d_copy_func_addr = get_device_function("__snax_kernel_idma_1d_copy");
     uint32_t __snax_kernel_gemm = get_device_function("__snax_kernel_gemm");
-    uint32_t __snax_kernel_minimal_cfg_start_gemm_and_wait_func_addr = get_device_function("__snax_kernel_minimal_cfg_start_gemm_and_wait");
     uint32_t __snax_kernel_check_results_func_addr = get_device_function("__snax_kernel_check_results");
     if (__snax_kernel_xdma_1d_copy_func_addr == SNAX_SYMTAB_END_FN_ADDR ||
         __snax_kernel_idma_1d_copy_func_addr == SNAX_SYMTAB_END_FN_ADDR ||
-        __snax_kernel_gemm == SNAX_SYMTAB_END_FN_ADDR || __snax_kernel_minimal_cfg_start_gemm_and_wait_func_addr == SNAX_SYMTAB_END_FN_ADDR ||
+        __snax_kernel_gemm == SNAX_SYMTAB_END_FN_ADDR ||
         __snax_kernel_check_results_func_addr == SNAX_SYMTAB_END_FN_ADDR)
     {
         HOST_DEBUG_PRINT("Error: Kernel symbol lookup failed!\r\n");
@@ -95,18 +95,19 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     // HOST_DEBUG_PRINT("Chip(%x, %x): D2 golden matrix L3 address: 0x%lx\r\n",
     //                  get_current_chip_loc_x(), get_current_chip_loc_y(), D2_addr_golden);
 
-    uint64_t cluster_l1_addr_A1 = bingo_l1_alloc(current_chip_id, 0, BINGO_CHIPLET_READW(A1dataSize));
+    uint64_t cluster_l1_addr_A1 = bingo_l1_alloc(assigned_chip_id, assigned_cluster_id, BINGO_CHIPLET_READW(A1dataSize));
 
-    uint64_t cluster_l1_addr_B1 = bingo_l1_alloc(current_chip_id, 0, BINGO_CHIPLET_READW(B1dataSize));
+    uint64_t cluster_l1_addr_B1 = bingo_l1_alloc(assigned_chip_id, assigned_cluster_id, BINGO_CHIPLET_READW(B1dataSize));
 
-    uint64_t cluster_l1_addr_D1 = bingo_l1_alloc(current_chip_id, 0, BINGO_CHIPLET_READW(D1dataSize));
+    uint64_t cluster_l1_addr_D1 = bingo_l1_alloc(assigned_chip_id, assigned_cluster_id, BINGO_CHIPLET_READW(D1dataSize));
+    uint64_t cluster_l1_addr_B2 = bingo_l1_alloc(assigned_chip_id, assigned_cluster_id, BINGO_CHIPLET_READW(B2dataSize));
 
-    uint64_t cluster_l1_addr_B2 = bingo_l1_alloc(current_chip_id, 0, BINGO_CHIPLET_READW(B2dataSize));
-
-    uint64_t cluster_l1_addr_D2 = bingo_l1_alloc(current_chip_id, 0, BINGO_CHIPLET_READW(D2datasize));
+    uint64_t cluster_l1_addr_D2 = bingo_l1_alloc(assigned_chip_id, assigned_cluster_id, BINGO_CHIPLET_READW(D2datasize));
 
     // Args for loading A1
-    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_A = (__snax_kernel_xdma_1d_copy_args_t *)o1heapAllocate(bingo_get_l3_heap_manager(get_current_chip_id()), sizeof(__snax_kernel_xdma_1d_copy_args_t));
+    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_A = (__snax_kernel_xdma_1d_copy_args_t *)bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_xdma_1d_copy_args_t));
     task_l3_to_cluster_args_A->src_addr_hi = HIGH32(BINGO_CHIPLET_READD(A1_addr_l3));
     task_l3_to_cluster_args_A->src_addr_lo = LOW32(BINGO_CHIPLET_READD(A1_addr_l3));
     task_l3_to_cluster_args_A->dst_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_A1));
@@ -114,10 +115,9 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     task_l3_to_cluster_args_A->size = ARRAY_SIZE_BYTES(A1);
 
     // Args for loading B1
-    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_B1 =
-        (__snax_kernel_xdma_1d_copy_args_t *)o1heapAllocate(
-            bingo_get_l3_heap_manager(current_chip_id),
-            sizeof(__snax_kernel_xdma_1d_copy_args_t));
+    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_B1 = (__snax_kernel_xdma_1d_copy_args_t *)bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_xdma_1d_copy_args_t));
     task_l3_to_cluster_args_B1->src_addr_hi = HIGH32(BINGO_CHIPLET_READD(B1_addr_l3));
     task_l3_to_cluster_args_B1->src_addr_lo = LOW32(BINGO_CHIPLET_READD(B1_addr_l3));
     task_l3_to_cluster_args_B1->dst_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_B1));
@@ -125,7 +125,10 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     task_l3_to_cluster_args_B1->size = ARRAY_SIZE_BYTES(B1);
 
     // args for gemm1
-    __snax_kernel_gemm_intra_chiplet_args_t *gemm1_args = (__snax_kernel_gemm_intra_chiplet_args_t *)o1heapAllocate(bingo_get_l3_heap_manager(get_current_chip_id()), sizeof(__snax_kernel_gemm_intra_chiplet_args_t));
+    __snax_kernel_gemm_intra_chiplet_args_t *gemm1_args = (__snax_kernel_gemm_intra_chiplet_args_t *) bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_gemm_intra_chiplet_args_t));
+    
     gemm1_args->input_A_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_A1));
     gemm1_args->input_A_addr_lo = LOW32(BINGO_CHIPLET_READD(cluster_l1_addr_A1));
     gemm1_args->input_B_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_B1));
@@ -143,10 +146,9 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     gemm1_args->accumPrevC = BINGO_CHIPLET_READW(accumPrevC); // false
 
     // args for loading B2
-    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_B2 =
-        (__snax_kernel_xdma_1d_copy_args_t *)o1heapAllocate(
-            bingo_get_l3_heap_manager(current_chip_id),
-            sizeof(__snax_kernel_xdma_1d_copy_args_t));
+    __snax_kernel_xdma_1d_copy_args_t *task_l3_to_cluster_args_B2 = (__snax_kernel_xdma_1d_copy_args_t *) bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_xdma_1d_copy_args_t));
     task_l3_to_cluster_args_B2->src_addr_hi = HIGH32(BINGO_CHIPLET_READD(B2_addr_l3));
     task_l3_to_cluster_args_B2->src_addr_lo = LOW32(BINGO_CHIPLET_READD(B2_addr_l3));
     task_l3_to_cluster_args_B2->dst_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_B2));
@@ -154,7 +156,9 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     task_l3_to_cluster_args_B2->size = ARRAY_SIZE_BYTES(B2);
 
     // args for gemm2
-    __snax_kernel_gemm_intra_chiplet_args_t *gemm2_args = (__snax_kernel_gemm_intra_chiplet_args_t *)o1heapAllocate(bingo_get_l3_heap_manager(get_current_chip_id()), sizeof(__snax_kernel_gemm_intra_chiplet_args_t));
+    __snax_kernel_gemm_intra_chiplet_args_t *gemm2_args = (__snax_kernel_gemm_intra_chiplet_args_t *) bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_gemm_intra_chiplet_args_t));
     gemm2_args->input_A_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_D1));
     gemm2_args->input_A_addr_lo = LOW32(BINGO_CHIPLET_READD(cluster_l1_addr_D1));
     gemm2_args->input_B_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_B2));
@@ -173,10 +177,9 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     // args for storing E
     uint64_t D2_addr_l3 = o1heapAllocate(bingo_get_l3_heap_manager(get_current_chip_id()), ARRAY_SIZE_BYTES(D2));
 
-    __snax_kernel_xdma_1d_copy_args_t *task_cluster_to_l3_args_D2 =
-        (__snax_kernel_xdma_1d_copy_args_t *)o1heapAllocate(
-            bingo_get_l3_heap_manager(current_chip_id),
-            sizeof(__snax_kernel_xdma_1d_copy_args_t));
+    __snax_kernel_xdma_1d_copy_args_t *task_cluster_to_l3_args_D2 = (__snax_kernel_xdma_1d_copy_args_t *) bingo_l3_alloc(
+        assigned_chip_id,
+        sizeof(__snax_kernel_xdma_1d_copy_args_t));
     task_cluster_to_l3_args_D2->src_addr_hi = HIGH32(BINGO_CHIPLET_READD(cluster_l1_addr_D2));
     task_cluster_to_l3_args_D2->src_addr_lo = LOW32(BINGO_CHIPLET_READD(cluster_l1_addr_D2));
     task_cluster_to_l3_args_D2->dst_addr_hi = HIGH32(BINGO_CHIPLET_READD(D2_addr_l3));
@@ -184,51 +187,50 @@ uint32_t __workload_gemm_stacked(bingo_task_t **task_list)
     task_cluster_to_l3_args_D2->size = ARRAY_SIZE_BYTES(D2);
 
     // args for checking results
-    __snax_kernel_check_results_args_t *task_check_results_args_D2 =
-        (__snax_kernel_check_results_args_t *)o1heapAllocate(
-            bingo_get_l3_heap_manager(current_chip_id),
+    __snax_kernel_check_results_args_t *task_check_results_args_D2 =(__snax_kernel_check_results_args_t *) bingo_l3_alloc(
+            assigned_chip_id,
             sizeof(__snax_kernel_check_results_args_t));
     task_check_results_args_D2->golden_data_addr = LOW32(BINGO_CHIPLET_READD(D2_addr_golden));
     task_check_results_args_D2->output_data_addr = LOW32(BINGO_CHIPLET_READD(D2_addr_l3));
-    task_check_results_args_D2->data_size = ARRAY_SIZE_BYTES(D2);
+    task_check_results_args_D2->data_size = 64;
 
     // register the tasks
     // 1. Load A1
     bingo_task_t *task_load_A1 = bingo_task_create(
         __snax_kernel_xdma_1d_copy_func_addr,
         (uint32_t)(uintptr_t)task_l3_to_cluster_args_A,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_load_B1 = bingo_task_create(
         __snax_kernel_xdma_1d_copy_func_addr,
         (uint32_t)(uintptr_t)task_l3_to_cluster_args_B1,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_gemm1 = bingo_task_create(
         __snax_kernel_gemm,
         (uint32_t)(uintptr_t)gemm1_args,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_load_B2 = bingo_task_create(
         __snax_kernel_xdma_1d_copy_func_addr,
         (uint32_t)(uintptr_t)task_l3_to_cluster_args_B2,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_gemm2 = bingo_task_create(
         __snax_kernel_gemm,
         (uint32_t)(uintptr_t)gemm2_args,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_store_D2 = bingo_task_create(
         __snax_kernel_xdma_1d_copy_func_addr,
         (uint32_t)(uintptr_t)task_cluster_to_l3_args_D2,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
     bingo_task_t *task_check_D2 = bingo_task_create(
         __snax_kernel_check_results_func_addr,
         (uint32_t)(uintptr_t)task_check_results_args_D2,
-        task_chip_id,
-        0);
+        assigned_chip_id,
+        assigned_cluster_id);
 
     // Set the task dependency
     bingo_task_add_depend(task_load_B1, task_load_A1);
@@ -261,21 +263,30 @@ int kernel_execution(){
     // Can be changed if needed
     bingo_task_t *task_list[64] = {0};
     uint32_t num_tasks = 0;
+    if (get_current_chip_id() == 0) {
+        ////////////////////////////
+        // User defined workload
+        ///////////////////////////
+        num_tasks = __workload_gemm_stacked(task_list);
+        ////////////////////////////
+        // End user defined workload
+        ////////////////////////////
 
-    num_tasks = __workload_gemm_stacked(task_list);
-    ////////////////////////////
-    // End user defined workload
-    ////////////////////////////
+        // Call bingo runtime
+        bingo_runtime_schedule(
+            task_list, 
+            num_tasks
+        );
+        printf("Chip(%x, %x): [Host] All tasks done.\n", get_current_chip_loc_x(), get_current_chip_loc_y());
+        // Close all the clusters
+        bingo_close_all_clusters(task_list, num_tasks);
+        // Free the output data
 
-    // Call bingo runtime
-    bingo_runtime_schedule(
-        task_list, 
-        num_tasks
-    );
-    printf("Chip(%x, %x): [Host] All tasks done.\n", get_current_chip_loc_x(), get_current_chip_loc_y());
-    // Close all the clusters
-    bingo_close_all_clusters(task_list, num_tasks);
-    // Free the output data
-
-    return 0;
+        return 0;
+    }
+    else
+    {
+        // other chiplets do nothing
+        return 0;
+    }
 }
