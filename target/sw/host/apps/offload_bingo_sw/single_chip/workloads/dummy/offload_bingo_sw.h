ec44421998e7589bd6b47e09e60eeb17ff5e9fff
Konste11ation <fanchen.kong@outlook.be>
2026-01-29 12:20:02 +0100

    update the offload_bingo_sw dummy workload

--- target/sw/host/apps/offload_bingo_sw/single_chip/workloads/dummy/offload_bingo_sw.h
+++ target/sw/host/apps/offload_bingo_sw/single_chip/workloads/dummy/offload_bingo_sw.h
@@ -13,20 +13,20 @@ uint32_t __workload_dummy(bingo_task_t **task_list) {
     check_kernel_tab_ready();
     uint32_t dummy_func_addr = get_device_function("__snax_kernel_dummy");
     if (dummy_func_addr == SNAX_SYMTAB_END_FN_ADDR) {
-        printf("Error: Kernel symbol lookup failed! Exiting\r\n");
+        printf_safe("Error: Kernel symbol lookup failed! Exiting\r\n");
         return 0;
     }
 
     // 2. Register the tasks
     bingo_task_t *task_dummy_0 = bingo_task_create(dummy_func_addr, (uint32_t)(uintptr_t)(&dummy_args_0),0,0);
-    bingo_task_t *task_dummy_1 = bingo_task_create(dummy_func_addr, (uint32_t)(uintptr_t)(&dummy_args_1),0,1);
-    bingo_task_t *task_dummy_2 = bingo_task_create(dummy_func_addr, (uint32_t)(uintptr_t)(&dummy_args_0),0,0);
+    bingo_task_t *task_dummy_1 = bingo_task_create(dummy_func_addr, (uint32_t)(uintptr_t)(&dummy_args_1),0,0);
+    bingo_task_t *task_dummy_2 = bingo_task_create(dummy_func_addr, (uint32_t)(uintptr_t)(&dummy_args_2),0,0);
 
     // 3. Set the task dependency
     //      T0 (cluster 0)
     //       |
     //       v
-    //      T1 (cluster 1)
+    //      T1 (cluster 0)
     //       |
     //       v
     //      T2 (cluster 0)
