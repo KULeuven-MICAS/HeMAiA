d88186ad38551deea0bf69910a6d738451d191fc
Yunhao Deng <IveanEx@outlook.com>
2026-02-02 14:04:46 +0100

    Fix clock divider frequency change glitch (#258)
    * Update hemaia_clock_divider
    
    * Fix 1-0 switch glitch
    
    * Change local ci to interposer-based simulator

--- hw/hemaia/hemaia_clk_rst_controller/hemaia_clock_divider.sv
+++ hw/hemaia/hemaia_clk_rst_controller/hemaia_clock_divider.sv
@@ -37,7 +37,8 @@ module hemaia_clock_counter #(
 endmodule
 
 
-(* no_ungroup *) (* no_boundary_optimization *) (* keep_hierarchy *) (* no_clock_gating *) (* KEEP_HIERARCHY = "TRUE" *)
+(* no_ungroup *) (* no_boundary_optimization *) (* keep_hierarchy *) (* no_clock_gating *)
+    (* KEEP_HIERARCHY = "TRUE" *)
 module hemaia_clock_divider #(
     parameter int MaxDivisionWidth = 4,
     parameter int DefaultDivision  = 1
@@ -52,12 +53,12 @@ module hemaia_clock_divider #(
 );
 
   logic [MaxDivisionWidth-1:0] divisor_q;
+  logic clk_gated;
   logic new_divisor_ready;
 
 
   // Counters for edges and local flip-flops
   logic [MaxDivisionWidth-1:0] cnt;
-  logic last_val;
 
   hemaia_clock_counter #(
       .Width(MaxDivisionWidth)
@@ -68,16 +69,18 @@ module hemaia_clock_divider #(
       .clear_i(1'b0),
       .ceiling_i(divisor_q),
       .count_o(cnt),
-      .last_value_o(last_val)
+      .last_value_o()
   );
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
       new_divisor_ready <= '0;
+      clk_gated <= '0;
       divisor_q <= DefaultDivision[MaxDivisionWidth-1:0];
     end else if (new_divisor_ready && (cnt == 0)) begin
       new_divisor_ready <= '0;
-      divisor_q <= divisor_i;
+      clk_gated <= (divisor_i == 0);
+      divisor_q <= (divisor_i != 0) ? divisor_i : divisor_q;
     end else if (divisor_valid_i) begin
       new_divisor_ready <= 1'b1;
     end
@@ -122,18 +125,28 @@ module hemaia_clock_divider #(
       .clk_o(clk_divided)
   );
 
+  logic clk_sel_o_mux;
+
+  always_ff @(negedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      clk_sel_o_mux <= 1'b0;
+    end else begin
+      clk_sel_o_mux <= (divisor_q == 'd1);
+    end
+  end
+
   (* DONT_TOUCH = "TRUE" *)
   tc_clk_mux2 i_clk_o_mux (
       .clk0_i(clk_divided),
       .clk1_i(clk_i),
-      .clk_sel_i(divisor_q == 'd1),
+      .clk_sel_i(clk_sel_o_mux),
       .clk_o(clk_ungated)
   );
 
   (* DONT_TOUCH = "TRUE" *)
   tc_clk_gating i_clk_o_gate (
       .clk_i(clk_ungated),
-      .en_i(divisor_q != '0 && rst_ni),
+      .en_i((~clk_gated) && rst_ni),
       .test_en_i(test_mode_i),
       .clk_o(clk_o)
   );
