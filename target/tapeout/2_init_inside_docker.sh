5bf982c614c453d940ecb336c9781262e1af4fb9
IveanEx <yunhao.deng@kuleuven.be>
2026-03-23 23:39:13 +0100

    Remove clean in tapeout script step 2

--- target/tapeout/2_init_inside_docker.sh
+++ target/tapeout/2_init_inside_docker.sh
@@ -7,7 +7,6 @@ CHIP_TYPE="single_chip"
 WORKLOAD=""
 DEV_APP="snax-test-integration"
 
-make -C $script_dir/../.. clean
 make -C $script_dir/../.. sw CFG_OVERRIDE=target/rtl/cfg/$cfg_name
 make -C $script_dir/../.. apps HOST_APP_TYPE=$HOST_APP_TYPE CHIP_TYPE=$CHIP_TYPE WORKLOAD=$WORKLOAD DEV_APP=$DEV_APP
 make -C $script_dir/../.. bootrom CFG_OVERRIDE=target/rtl/cfg/$cfg_name
