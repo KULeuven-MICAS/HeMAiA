ce08b636e3f9399c6ac89cb6eeb673af7896c6e2
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2026-06-02 11:24:37 +0200

    init gemm presure test

---
+++ target/testing/versacore/testing_launch.py
@@ -0,0 +1,5 @@
+# read each row of the testing_workload.csv file and generate the corresponding test case (the prams.hjson file) for the gemm mem chip 1 cluster workload. 
+# then compile and run the test case and collect the results. 
+# finally, save the results to a csv file.
+# reuse the code in the testing_workload.py file to generate the testing_workload.csv file.
+# reuse the code in test_util.py to compile sw, hw, etd and run the test case i=recuirsively and collect the results. 
