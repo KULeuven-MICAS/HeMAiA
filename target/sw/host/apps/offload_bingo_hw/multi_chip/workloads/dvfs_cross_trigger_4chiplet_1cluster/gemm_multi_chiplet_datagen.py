f5ea2958dd1ebde9f44ff327b8e8f3cdc2ea892e
Konste11ation <fanchen.kong@outlook.be>
2026-07-05 01:13:20 +0200

    add the dvfs testing sw

---
+++ target/sw/host/apps/offload_bingo_hw/multi_chip/workloads/dvfs_cross_trigger_4chiplet_1cluster/gemm_multi_chiplet_datagen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Copyright 2026 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Xiaoling Yi <xiaoling.yi@kuleuven.be>
+
+import os
+import sys
+
+SIM_UTIL_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../../../../../../../util/sim")
+)
+if SIM_UTIL_DIR not in sys.path:
+    sys.path.append(SIM_UTIL_DIR)
+
+import _usg_paths  # noqa: F401,E402  (registers util/sim/{common,gemm,xdma,ara} on sys.path)
+from gemm_sim_utils import (  # noqa E402
+    emit_multichip_gemm_header_file,
+    run_multichip_gemm_datagen,
+)
+
+
+def emit_header_file(**kwargs):
+    return emit_multichip_gemm_header_file(**kwargs)
+
+
+def main():
+    run_multichip_gemm_datagen()
+
+
+if __name__ == "__main__":
+    main()
