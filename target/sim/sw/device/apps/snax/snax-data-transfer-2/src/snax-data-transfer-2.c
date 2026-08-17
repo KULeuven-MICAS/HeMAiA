a1971b76c8e7f3b62931d457a92139bacda6e48c
Ryan Antonio <ryan.albert.antonio@eee.upd.edu.ph>
2026-08-17 15:26:41 +0200

    Ry/add data syncs (#352)
    * snax-data transfer sets
    
    * snax-data transfer sets

---
+++ target/sim/sw/device/apps/snax/snax-data-transfer-2/src/snax-data-transfer-2.c
@@ -0,0 +1,148 @@
+// Copyright 2024 KU Leuven.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//-------------------------------
+// Author: Ryan Antonio <ryan.antonio@esat.kuleuven.be>
+//
+// Program: Hypercorex Test CSRs
+//
+// This program is to test the capabilities
+// of the HyperCoreX accelerator's CSRs so the test is
+// to check if registers are working as intended.
+//
+// This includes checking for RW, RO, and WO registers.
+//-------------------------------
+
+#include "snrt.h"
+
+#include "data.h"
+
+#define PRINT_ADDR 0
+
+// Addresses
+uint32_t *tcdm_c0_in;
+uint32_t *tcdm_c0_out;
+
+uint32_t *tcdm_c1_in;
+uint32_t *tcdm_c1_out;
+
+uint32_t dma_start_c0;
+uint32_t dma_end_c0;
+
+uint32_t dma_start_c1;
+uint32_t dma_end_c1;
+
+// uint32_t dma_start_c2;
+// uint32_t dma_end_c2;
+
+int main() {
+
+    // Set err value for checking
+    int err = 0;
+
+    //----------------------------
+    // Pre-load stages
+    // Needs to be done sequentially
+    //----------------------------
+
+    // Cluster 0
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+            tcdm_c0_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c0_out = tcdm_c0_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c0_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c0_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    // Cluster 1
+    if (snrt_cluster_idx() == 1){
+
+       if(snrt_is_dm_core()){
+            tcdm_c1_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c1_out = tcdm_c1_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c1_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c1_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    };
+
+    snrt_global_barrier();
+
+
+    //----------------------------
+    // Parallel transfer stages
+    // Input-output parallel pipeline stages
+    // First cluster pulls form L2 memory
+    // Succeeding clusters pull continuously from the previous cluster's output
+    //----------------------------
+
+    // Cluster 0
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+
+            dma_start_c0 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c0_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c0 = snrt_mcycle();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    // Cluster 1
+    if (snrt_cluster_idx() == 1) {
+        if(snrt_is_dm_core()){
+
+            dma_start_c1 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c1_in, tcdm_c0_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c1 = snrt_mcycle();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+
+    //----------------------------
+    // Printing sessions
+    //----------------------------
+
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+            printf_safe("C0DM %d  \r\n", dma_end_c0 - dma_start_c0);
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 1) {
+        if(snrt_is_dm_core()){
+            printf_safe("C1DM %d  \r\n", dma_end_c1 - dma_start_c1);
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    return err;
+}
---
+++ target/sim/sw/device/apps/snax/snax-data-transfer-8/src/snax-data-transfer-8.c
@@ -0,0 +1,418 @@
+// Copyright 2024 KU Leuven.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//-------------------------------
+// Author: Ryan Antonio <ryan.antonio@esat.kuleuven.be>
+//
+// Program: Hypercorex Test CSRs
+//
+// This program is to test the capabilities
+// of the HyperCoreX accelerator's CSRs so the test is
+// to check if registers are working as intended.
+//
+// This includes checking for RW, RO, and WO registers.
+//-------------------------------
+
+#include "snrt.h"
+
+#include "data.h"
+
+#define PRINT_ADDR 0
+
+// Addresses
+uint32_t *tcdm_c0_in;
+uint32_t *tcdm_c0_out;
+
+uint32_t *tcdm_c1_in;
+uint32_t *tcdm_c1_out;
+
+uint32_t *tcdm_c2_in;
+uint32_t *tcdm_c2_out;
+
+uint32_t *tcdm_c3_in;
+uint32_t *tcdm_c3_out;
+
+uint32_t *tcdm_c4_in;
+uint32_t *tcdm_c4_out;
+
+uint32_t *tcdm_c5_in;
+uint32_t *tcdm_c5_out;
+
+uint32_t *tcdm_c6_in;
+uint32_t *tcdm_c6_out;
+
+uint32_t *tcdm_c7_in;
+uint32_t *tcdm_c7_out;
+
+uint32_t dma_start_c0;
+uint32_t dma_end_c0;
+
+uint32_t dma_start_c1;
+uint32_t dma_end_c1;
+
+uint32_t dma_start_c2;
+uint32_t dma_end_c2;
+
+uint32_t dma_start_c3;
+uint32_t dma_end_c3;
+
+uint32_t dma_start_c4;
+uint32_t dma_end_c4;
+
+uint32_t dma_start_c5;
+uint32_t dma_end_c5;
+
+uint32_t dma_start_c6;
+uint32_t dma_end_c6;
+
+uint32_t dma_start_c7;
+uint32_t dma_end_c7;
+
+int main() {
+
+    // Set err value for checking
+    int err = 0;
+
+    //----------------------------
+    // Pre-load stages
+    // Needs to be done sequentially
+    //----------------------------
+
+    // Cluster 0
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+            tcdm_c0_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c0_out = tcdm_c0_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c0_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c0_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    // Cluster 1
+    if (snrt_cluster_idx() == 1){
+
+       if(snrt_is_dm_core()){
+            tcdm_c1_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c1_out = tcdm_c1_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c1_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c1_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    };
+
+    snrt_global_barrier();
+
+    // Cluster 2
+    if (snrt_cluster_idx() == 2){
+
+       if(snrt_is_dm_core()){
+            tcdm_c2_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c2_out = tcdm_c2_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c2_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c2_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    };
+
+    snrt_global_barrier();
+
+    // Cluster 3
+    if (snrt_cluster_idx() == 3){
+         if(snrt_is_dm_core()){
+            tcdm_c3_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c3_out = tcdm_c3_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c3_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c3_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+          };
+    
+          snrt_cluster_hw_barrier();
+     };
+
+    snrt_global_barrier();
+
+    // Cluster 4
+    if (snrt_cluster_idx() == 4){
+       if(snrt_is_dm_core()){
+            tcdm_c4_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c4_out = tcdm_c4_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c4_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c4_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    };
+
+    snrt_global_barrier();
+
+    // Cluster 5
+    if (snrt_cluster_idx() == 5){
+       if(snrt_is_dm_core()){
+            tcdm_c5_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c5_out = tcdm_c5_in + half_super_bank; 
+
+            snrt_dma_start_1d(tcdm_c5_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c5_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+    
+        snrt_cluster_hw_barrier();
+    
+    };
+
+    snrt_global_barrier();
+
+    // Cluster 6
+    if (snrt_cluster_idx() == 6){
+       if(snrt_is_dm_core()){
+            tcdm_c6_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c6_out = tcdm_c6_in + half_super_bank;
+
+            snrt_dma_start_1d(tcdm_c6_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c6_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+
+        snrt_cluster_hw_barrier();
+    };
+
+    snrt_global_barrier();
+
+    // Cluster 7
+    if (snrt_cluster_idx() == 7){
+       if(snrt_is_dm_core()){
+            tcdm_c7_in = (uint32_t*)snrt_cluster_base_addrl();
+            tcdm_c7_out = tcdm_c7_in + half_super_bank; 
+
+            snrt_dma_start_1d(tcdm_c7_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+
+            snrt_dma_start_1d(tcdm_c7_out, data_set, num_bytes);
+            snrt_dma_wait_all();
+        };
+    
+        snrt_cluster_hw_barrier();
+    
+    };
+
+    snrt_global_barrier();
+
+
+    //----------------------------
+    // Parallel transfer stages
+    // Input-output parallel pipeline stages
+    // First cluster pulls form L2 memory
+    // Succeeding clusters pull continuously from the previous cluster's output
+    //----------------------------
+
+    // Cluster 0
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+
+            dma_start_c0 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c0_in, data_set, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c0 = snrt_mcycle();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    // Cluster 1
+    if (snrt_cluster_idx() == 1) {
+        if(snrt_is_dm_core()){
+
+            dma_start_c1 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c1_in, tcdm_c0_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c1 = snrt_mcycle();
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    // Cluster 2
+    if (snrt_cluster_idx() == 2) {
+        if(snrt_is_dm_core()){
+            
+            dma_start_c2 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c2_in, tcdm_c1_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c2 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    // Cluster 3
+    if (snrt_cluster_idx() == 3) {
+        if(snrt_is_dm_core()){
+            dma_start_c3 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c3_in, tcdm_c2_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c3 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    // Cluster 4
+    if (snrt_cluster_idx() == 4) {
+        if(snrt_is_dm_core()){
+            dma_start_c4 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c4_in, tcdm_c3_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c4 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    // Cluster 5
+    if (snrt_cluster_idx() == 5) {
+        if(snrt_is_dm_core()){
+            dma_start_c5 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c5_in, tcdm_c4_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c5 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    // Cluster 6
+    if (snrt_cluster_idx() == 6) {
+        if(snrt_is_dm_core()){
+            dma_start_c6 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c6_in, tcdm_c5_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c6 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    // Cluster 7
+    if (snrt_cluster_idx() == 7) {
+        if(snrt_is_dm_core()){
+            dma_start_c7 = snrt_mcycle();
+            snrt_dma_start_1d(tcdm_c7_in, tcdm_c6_out, num_bytes);
+            snrt_dma_wait_all();
+            dma_end_c7 = snrt_mcycle();
+        };
+        snrt_cluster_hw_barrier();
+    }
+
+    //----------------------------
+    // Printing sessions
+    //----------------------------
+
+    snrt_global_barrier();
+    
+    if (snrt_cluster_idx() == 0) {
+        if(snrt_is_dm_core()){
+            printf_safe("C0DM %d  \r\n", dma_end_c0 - dma_start_c0);
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 1) {
+        if(snrt_is_dm_core()){
+            printf_safe("C1DM %d  \r\n", dma_end_c1 - dma_start_c1);
+        };
+
+        snrt_cluster_hw_barrier();
+    } 
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 2) {
+        if(snrt_is_dm_core()){
+            printf_safe("C2DM %d  \r\n", dma_end_c2 - dma_start_c2);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 3) {
+        if(snrt_is_dm_core()){
+            printf_safe("C3DM %d  \r\n", dma_end_c3 - dma_start_c3);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 4) {
+        if(snrt_is_dm_core()){
+            printf_safe("C4DM %d  \r\n", dma_end_c4 - dma_start_c4);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 5) {
+        if(snrt_is_dm_core()){
+            printf_safe("C5DM %d  \r\n", dma_end_c5 - dma_start_c5);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 6) {
+        if(snrt_is_dm_core()){
+            printf_safe("C6DM %d  \r\n", dma_end_c6 - dma_start_c6);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    if (snrt_cluster_idx() == 7) {
+        if(snrt_is_dm_core()){
+            printf_safe("C7DM %d  \r\n", dma_end_c7 - dma_start_c7);
+        };
+
+        snrt_cluster_hw_barrier();
+    }
+
+    snrt_global_barrier();
+
+    return err;
+}
