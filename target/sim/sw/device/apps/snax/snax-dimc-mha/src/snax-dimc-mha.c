498a271cec155c452f4badf272590b5aa909922f
rgantonio <ryan.antonio@esat.kuleuven.be>
2026-08-25 09:01:26 +0200

    sw: improve original dimc mha program

--- target/sim/sw/device/apps/snax/snax-dimc-mha/src/snax-dimc-mha.c
+++ target/sim/sw/device/apps/snax/snax-dimc-mha/src/snax-dimc-mha.c
@@ -16,7 +16,7 @@ int main() {
     int err = 0;
     if (snrt_cluster_idx() == 3){
 
-        // printf("Starting the DIMC test\r\n");
+        // printf_safe("Starting the DIMC test\r\n");
 
         // allocate 32+32+32KB in TCDM for activation and weight pair
         uint64_t *activation_ptr, *weight_ptr, *output_ptr;
@@ -35,13 +35,13 @@ int main() {
         // load WK, K, Q to TCDM
 
         if (snrt_is_dm_core()) {
-            printf("DMA core is configured for K and WK\r\n");
+            printf_safe("DMA core is configured for K and WK\r\n");
 
             // measure the start of cycle count for preloading data to TCDM
             uint32_t start_dma_load = snrt_mcycle();
 
             // initialize TCDM with matrix K by DMA
-            printf("INITIALIZING TCDM\r\n");
+            printf_safe("INITIALIZING TCDM\r\n");
 
             // read weight WK and ativation K from data.h
             size_t vector_size = Q_LENGTH * sizeof(uint64_t);
@@ -54,7 +54,7 @@ int main() {
 
             // measures the end of the DMA transfer process
             uint32_t end_dma_load = snrt_mcycle();
-            printf("DMA core exits after loading K and WK\r\n"); 
+            printf_safe("DMA core exits after loading K and WK\r\n"); 
         }
 
         /**************************************************************************/
@@ -66,31 +66,31 @@ int main() {
         /**************************************************************************/
 
         if (snrt_is_compute_core()){
-            printf("COMPUTE CORE is configured\r\n");
+            printf_safe("COMPUTE CORE is configured\r\n");
 
             // configure the accelerator
-            printf("ENTERING MHA MODE\r\n");
+            printf_safe("ENTERING MHA MODE\r\n");
 
             uint32_t busy = dimc_query_busy();
-            printf("%d: busy\r\n", busy);
-            printf("QUERYING BUSY SUCCEEDED\r\n");
+            printf_safe("%d: busy\r\n", busy);
+            printf_safe("QUERYING BUSY SUCCEEDED\r\n");
 
             configure_accelerator();
 
-            printf("CONFIGURING ACCELERATOR SUCCEEDED\r\n");
+            printf_safe("CONFIGURING ACCELERATOR SUCCEEDED\r\n");
 
             uint32_t read_zp_qkv = read_zp();
-            printf("%d: read_zp_qkv\r\n", read_zp_qkv);
-            printf("READING ZP SUCCEEDED\r\n");
+            printf_safe("%d: read_zp_qkv\r\n", read_zp_qkv);
+            printf_safe("READING ZP SUCCEEDED\r\n");
 
             // send WK
-            printf("CONFIGURING STREAMERS for WK\r\n");
+            printf_safe("CONFIGURING STREAMERS for WK\r\n");
             dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-            printf("STREAMER CONFIGURED FOR WK\r\n");
+            printf_safe("STREAMER CONFIGURED FOR WK\r\n");
 
             // configure the accelerator to start MHA computation
             dimc_start_mha();
@@ -111,7 +111,7 @@ int main() {
         /**************************************************************************/
 
         if (snrt_is_dm_core()) {
-            printf("DMA core is configured for WQ\r\n");
+            printf_safe("DMA core is configured for WQ\r\n");
 
             // read weight WQ from data.h
             size_t vector_size = Q_LENGTH * sizeof(uint64_t);
@@ -123,13 +123,13 @@ int main() {
 
         if (snrt_is_compute_core()){
             // send K
-            printf("CONFIGURING STREAMERS for K\r\n");
+            printf_safe("CONFIGURING STREAMERS for K\r\n");
             dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 24));
-            printf("STREAMER CONFIGURED FOR K\r\n");
+            printf_safe("STREAMER CONFIGURED FOR K\r\n");
 
             dimc_start_streamer();
 
@@ -146,13 +146,13 @@ int main() {
 
         if(snrt_is_compute_core()) {
             // send WQ
-            printf("CONFIGURING STREAMERS for WQ\r\n");
+            printf_safe("CONFIGURING STREAMERS for WQ\r\n");
             dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-            printf("STREAMER CONFIGURED for WQ\r\n");
+            printf_safe("STREAMER CONFIGURED for WQ\r\n");
 
             dimc_start_streamer();
 
@@ -169,7 +169,7 @@ int main() {
         /**************************************************************************/
 
         if (snrt_is_dm_core()) {
-            printf("DMA core is configured for WQ\r\n");
+            printf_safe("DMA core is configured for WQ\r\n");
 
             // read weight WK and ativation K from data.h
             size_t vector_size = Q_LENGTH * sizeof(uint64_t);
@@ -182,13 +182,13 @@ int main() {
 
         if (snrt_is_compute_core()){
             // send Q
-            printf("CONFIGURING STREAMERS for Q\r\n");
+            printf_safe("CONFIGURING STREAMERS for Q\r\n");
             dimc_set_streamer_dim_w(64, 1, 64, 0, 8, (uint32_t)(buffer_ptr));
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 24));
-            printf("STREAMER CONFIGURED for Q\r\n");
+            printf_safe("STREAMER CONFIGURED for Q\r\n");
 
             dimc_start_streamer();
 
@@ -205,13 +205,13 @@ int main() {
 
         if (snrt_is_compute_core()){
             // send V
-            printf("CONFIGURING STREAMERS for V\r\n");
+            printf_safe("CONFIGURING STREAMERS for V\r\n");
             dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 24));
-            printf("STREAMER CONFIGURED for V\r\n");
+            printf_safe("STREAMER CONFIGURED for V\r\n");
 
             dimc_start_streamer();
 
@@ -228,13 +228,13 @@ int main() {
 
         if (snrt_is_compute_core()) {
             // send WV
-            printf("CONFIGURING STREAMERS for WV\r\n");
+            printf_safe("CONFIGURING STREAMERS for WV\r\n");
             dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
             dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
             dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
             dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
             dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-            printf("STREAMER CONFIGURED for WV\r\n");
+            printf_safe("STREAMER CONFIGURED for WV\r\n");
 
             dimc_start_streamer();
 
@@ -252,19 +252,19 @@ int main() {
 
         if (snrt_is_compute_core()) {
             // send Q1K1T
-            printf("CONFIGURING STREAMERS for Q1K1T\r\n");
+            printf_safe("CONFIGURING STREAMERS for Q1K1T\r\n");
             dimc_set_streamer_dim_w(64, 1, 64, 0, 8, (uint32_t)(output_ptr));
             dimc_set_streamer_dim_r0(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr));
             dimc_set_streamer_dim_r1(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 8));
             dimc_set_streamer_dim_r2(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 16));
             dimc_set_streamer_dim_r3(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 24));
-            printf("STREAMER CONFIGURED for Q1K1T\r\n");
+            printf_safe("STREAMER CONFIGURED for Q1K1T\r\n");
 
             dimc_start_streamer();
 
             while (dimc_is_streamer_busy()) { }
 
-            printf("CHECK FINAL RESULT\r\n");
+            printf_safe("CHECK FINAL RESULT\r\n");
 
             // check the final result
             for (int i = 0; i < 512; ++i) {
@@ -275,17 +275,21 @@ int main() {
                 // Split each uint64_t element into 8 uint8_t elements
                 for (int j = 0; j < 8; ++j) {
                     uint8_t tmp_res = (uint8_t)((value >> (j * 8)) & 0xFF);
-                    // printf("%d ", tmp_res);
+                    // printf_safe("%d ", tmp_res);
                     if(tmp_res != gold[index + j]) {
-                        printf("MISMATCH at %d, res:%d, gold:%d\r\n", (index + j), tmp_res, gold[index + j]);
+                        printf_safe("MISMATCH at %d, res:%d, gold:%d\r\n", (index + j), tmp_res, gold[index + j]);
                         err += 1;
                     }
                 }
-                // printf("RESULTS MATCH WITH GOLDEN MODEL\r\n");
+                // printf_safe("RESULTS MATCH WITH GOLDEN MODEL\r\n");
             }
             
-            // printf("MIMATCH COUNT %d\r\n", err);
+            // printf_safe("PASS\r\n");
+            // printf_safe("MIMATCH COUNT %d\r\n", err);
+            
         }
+
+        snrt_cluster_hw_barrier();
+        return_to_cva6_single_cluster(err);
     }
-    return err;
 }   
